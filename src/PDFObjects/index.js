0611133c9ebed6d8e86b03b98763f3d32a908ac9
Andrew Dillon <andrew.dillon.j@gmail.com>
2017-10-08 21:25:55 -0500

    Remove old PDFObjects

--- src/PDFObjects/index.js
+++
@@ -1,8 +0,0 @@
-export { default as PDFArrayObject } from './PDFArrayObject';
-export { default as PDFDictionaryObject } from './PDFDictionaryObject';
-export { default as PDFNameObject } from './PDFNameObject';
-export { default as PDFIndirectObject } from './PDFIndirectObject';
-export { default as PDFStreamObject } from './PDFStreamObject';
-export { default as PDFTextObject } from './PDFTextObject';
-export { default as PDFCrossRef } from './PDFCrossRefTable';
-export { default as PDFTrailer } from './PDFTrailer';
