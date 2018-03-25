be260b7e91d35fd3c7d80d2b206eb36c41933aa2
Andrew Dillon <andrew.dillon.j@gmail.com>
2018-03-25 16:48:16 -0500

    Add unit test coverage reporter

---
+++ __tests__/core/pdf-parser/parseArray.spec.ts
@@ -0,0 +1,7 @@
+import parseArray from 'core/pdf-parser/parseArray';
+
+describe(`parseArray`, () => {
+  it(`does stuff`, () => {
+    expect(true).toBe(false);
+  });
+});
--- __tests__/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe(`index`, () => {
-  it(`does a thing...`, () => {
-    expect(true).toBe(false);
-  });
-});
