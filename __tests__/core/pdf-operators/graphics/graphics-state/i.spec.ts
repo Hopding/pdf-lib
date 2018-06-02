a32071b997e1d367a8fee12d0f964e10c65290a4
Andrew Dillon <andrew.dillon.j@gmail.com>
2018-06-02 08:22:48 -0500

    Add tests for i operator

---
+++ __tests__/core/pdf-operators/graphics/graphics-state/i.spec.ts
@@ -0,0 +1,55 @@
+// This is required to prevent an error due to circular imports in this test
+import 'core/pdf-objects';
+
+import i from 'core/pdf-operators/graphics/graphics-state/i';
+import PDFOperator from 'core/pdf-operators/PDFOperator';
+import { typedArrayFor } from 'utils';
+
+describe(`i`, () => {
+  it(`requires a Number to be constucted`, () => {
+    expect(() => new i()).toThrowError();
+    expect(() => new i(-0.1)).toThrowError(
+      'i operator arg "flatness" must be a number from 0 to 100.',
+    );
+    expect(() => new i(100.1)).toThrowError(
+      'i operator arg "flatness" must be a number from 0 to 100.',
+    );
+    expect(new i(0)).toBeInstanceOf(i);
+    expect(new i(100)).toBeInstanceOf(PDFOperator);
+  });
+
+  it(`has a static "of" method`, () => {
+    expect(() => i.of()).toThrowError();
+    expect(() => i.of(-0.1)).toThrowError(
+      'i operator arg "flatness" must be a number from 0 to 100.',
+    );
+    expect(() => i.of(100.1)).toThrowError(
+      'i operator arg "flatness" must be a number from 0 to 100.',
+    );
+    expect(i.of(0)).toBeInstanceOf(i);
+    expect(i.of(100)).toBeInstanceOf(PDFOperator);
+  });
+
+  describe(`"toString" method`, () => {
+    it(`returns the i instance as a string`, () => {
+      const instance = i.of(50);
+      expect(instance.toString()).toEqual('50 i\n');
+    });
+  });
+
+  describe(`"bytesSize" method`, () => {
+    it(`returns the size of the Singleton in bytes`, () => {
+      const instance = i.of(0);
+      expect(instance.bytesSize()).toEqual(4);
+    });
+  });
+
+  describe(`"copyBytesInto" method`, () => {
+    it(`copies the Singleton into the buffer as bytes`, () => {
+      const instance = i.of(100);
+      const buffer = new Uint8Array(instance.bytesSize());
+      instance.copyBytesInto(buffer);
+      expect(buffer).toEqual(typedArrayFor('100 i\n'));
+    });
+  });
+});
--- src/core/pdf-operators/graphics/graphics-state/i.ts
+++ src/core/pdf-operators/graphics/graphics-state/i.ts
@@ -5,9 +5,9 @@ import { addStringToBuffer } from 'utils';
 import { isInRange, validate } from 'utils/validate';
 
 /**
- * Set the flatness tolerance in the graphics state. flatness is a number in the
- * range 0 to 100; a value of 0 shall specify the output device’s default flatness
- * tolerance.
+ * Set the flatness tolerance in the graphics state.
+ * flatness is a number in the * range 0 to 100; a value of 0 shall specify the
+ *   output device’s default flatness tolerance.
  */
 class i extends PDFOperator {
   static of = (flatness: number) => new i(flatness);
@@ -18,15 +18,15 @@ class i extends PDFOperator {
     super();
     validate(
       flatness,
-      isInRange(0, 101),
+      isInRange(0, 100),
       'i operator arg "flatness" must be a number from 0 to 100.',
     );
     this.flatness = flatness;
   }
 
-  toString = (): string => `${this.flatness} i\n`;
+  toString = () => `${this.flatness} i\n`;
 
-  bytesSize = (): number => this.toString().length;
+  bytesSize = () => this.toString().length;
 
   copyBytesInto = (buffer: Uint8Array): Uint8Array =>
     addStringToBuffer(this.toString(), buffer);
