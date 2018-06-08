2f8697349a330fcd7d2e01484c4f1f6c11468281
Andrew Dillon <andrew.dillon.j@gmail.com>
2018-06-07 21:15:03 -0500

    Finish integration test2

--- __integration_tests__/tests/test2.ts
+++ __integration_tests__/tests/test2.ts
@@ -142,7 +142,6 @@ const makePage1ContentStream = (pdfDoc: PDFDocument, size: number) =>
       size: size / 2,
       fillRgbColor: [0.8, 0.8, 0.8],
     }),
-    // lineWidth()
     lineJoin(1),
     ...drawSquare({
       x: 50,
@@ -161,6 +160,113 @@ const makePage1ContentStream = (pdfDoc: PDFDocument, size: number) =>
       borderWidth: 15,
     }),
     popGraphicsState(),
+  );
+
+const makePage2ContentStream = (pdfDoc: PDFDocument, size: number) =>
+  PDFContentStream.of(
+    PDFDictionary.from({}, pdfDoc.index),
+
+    ...drawSquare({
+      size,
+      fillRgbColor: [253 / 255, 246 / 255, 227 / 255],
+    }),
+    ...drawLinesOfText(ipsumLines, {
+      y: size - 20,
+      size: 20,
+      font: '/Ubuntu-R',
+      fillRgbColor: [101 / 255, 123 / 255, 131 / 255],
+    }),
+    ...drawLinesOfText(ipsumLines, {
+      y: size - 105,
+      size: 25,
+      font: '/Fantasque-BI',
+      fillRgbColor: [101 / 255, 123 / 255, 131 / 255],
+    }),
+    ...drawLinesOfText(ipsumLines, {
+      y: size - 200,
+      size: 25,
+      font: '/IndieFlower-R',
+      fillRgbColor: [101 / 255, 123 / 255, 131 / 255],
+    }),
+    ...drawLinesOfText(ipsumLines, {
+      y: size - 300,
+      size: 30,
+      font: '/GreatVibes-R',
+      fillRgbColor: [101 / 255, 123 / 255, 131 / 255],
+    }),
+    ...drawLinesOfText(ipsumLines, {
+      y: size - 425,
+      size: 25,
+      font: '/AppleStorm-R',
+      fillRgbColor: [101 / 255, 123 / 255, 131 / 255],
+    }),
+    ...drawLinesOfText(ipsumLines, {
+      y: size - 500,
+      size: 15,
+      font: '/BioRhyme-R',
+      fillRgbColor: [101 / 255, 123 / 255, 131 / 255],
+    }),
+    ...drawLinesOfText(ipsumLines, {
+      y: size - 575,
+      size: 15,
+      font: '/PressStart2P-R',
+      fillRgbColor: [101 / 255, 123 / 255, 131 / 255],
+    }),
+    ...drawLinesOfText(ipsumLines, {
+      y: size - 650,
+      size: 25,
+      font: '/Hussar3D-R',
+      fillRgbColor: [101 / 255, 123 / 255, 131 / 255],
+    }),
+  );
+
+const makePage3ContentStream = (pdfDoc: PDFDocument, pageHeight: number) =>
+  PDFContentStream.of(
+    PDFDictionary.from({}, pdfDoc.index),
+
+    ...drawImage({
+      name: '/CatRidingUnicorn',
+      y: pageHeight - 1080 * 0.2,
+      width: 1920 * 0.2,
+      height: 1080 * 0.2,
+    }),
+    ...drawImage({
+      name: '/MinionsLaughing',
+      y: pageHeight - 1080 * 0.2 - 354 * 0.75,
+      width: 630 * 0.75,
+      height: 354 * 0.75,
+    }),
+    ...drawImage({
+      name: '/GreyscaleBird',
+      y: pageHeight - 1080 * 0.2 - 354 * 0.75 - 375 * 0.75,
+      width: 600 * 0.75,
+      height: 375 * 0.75,
+    }),
+    ...drawRectangle({
+      width: 960 * 0.49,
+      height: pageHeight - 1080 * 0.2 - 354 * 0.75 - 375 * 0.75,
+      fillRgbColor: [0, 1, 0],
+    }),
+    ...drawImage({
+      name: '/MinionsBananaAlpha',
+      y: pageHeight - 1080 * 0.2 - 354 * 0.75 - 375 * 0.75 - 640 * 0.5,
+      width: 960 * 0.5,
+      height: 640 * 0.5,
+    }),
+    ...drawImage({
+      name: '/MinionsBananaNoAlpha',
+      // prettier-ignore
+      y: pageHeight - 1080 * 0.2 - 354 * 0.75 - 375 * 0.75 - 640 * 0.5 - 640 * 0.5,
+      width: 960 * 0.5,
+      height: 640 * 0.5,
+    }),
+    ...drawImage({
+      name: '/SmallMario',
+      // prettier-ignore
+      y: pageHeight - 1080 * 0.2 - 354 * 0.75 - 375 * 0.75 - 640 * 0.5 - 640 * 0.5 - 1854 * 0.18,
+      width: 1473 * 0.18,
+      height: 1854 * 0.18,
+    }),
   );
 
 // Define the test kernel using the above content stream functions.
@@ -205,41 +311,41 @@ const kernel: IPDFCreator = (assets: ITestAssets) => {
     .addFontDictionary('FontTimesRoman', FontTimesRoman)
     .addContentStreams(page1ContentStreamRef);
 
-  // const page2Size = 750;
-  // const page2ContentStream = makePage2ContentStream(pdfDoc, page2Size);
-  // const page2ContentStreamRef = pdfDoc.register(page2ContentStream);
-  //
-  // const page2 = pdfDoc
-  //   .createPage([page2Size, page2Size])
-  //   .addFontDictionary('Ubuntu-R', FontUbuntuR)
-  //   .addFontDictionary('BioRhyme-R', FontBioRhymeR)
-  //   .addFontDictionary('PressStart2P-R', FontPressStart2PR)
-  //   .addFontDictionary('IndieFlower-R', FontIndieFlowerR)
-  //   .addFontDictionary('GreatVibes-R', FontGreatVibesR)
-  //   .addFontDictionary('Fantasque-BI', FontFantasqueBI)
-  //   .addFontDictionary('AppleStorm-R', FontAppleStormR)
-  //   .addFontDictionary('Hussar3D-R', FontHussar3D)
-  //   .addContentStreams(page2ContentStreamRef);
-  //
-  // const page3Width = 750;
-  // const page3Height = 1750;
-  // const page3ContentStream = makePage3ContentStream(pdfDoc, page3Height);
-  // const page3ContentStreamRef = pdfDoc.register(page3ContentStream);
-  //
-  // const page3 = pdfDoc
-  //   .createPage([page3Width, page3Height])
-  //   .addXObject('CatRidingUnicorn', JpgCatRidingUnicorn)
-  //   .addXObject('MinionsLaughing', JpgMinionsLaughing)
-  //   .addXObject('GreyscaleBird', PngGreyscaleBird)
-  //   .addXObject('MinionsBananaAlpha', PngMinionsBananaAlpha)
-  //   .addXObject('MinionsBananaNoAlpha', PngMinionsBananaNoAlpha)
-  //   .addXObject('SmallMario', PngSmallMario)
-  //   .addContentStreams(page3ContentStreamRef);
+  const page2Size = 750;
+  const page2ContentStream = makePage2ContentStream(pdfDoc, page2Size);
+  const page2ContentStreamRef = pdfDoc.register(page2ContentStream);
+
+  const page2 = pdfDoc
+    .createPage([page2Size, page2Size])
+    .addFontDictionary('Ubuntu-R', FontUbuntuR)
+    .addFontDictionary('BioRhyme-R', FontBioRhymeR)
+    .addFontDictionary('PressStart2P-R', FontPressStart2PR)
+    .addFontDictionary('IndieFlower-R', FontIndieFlowerR)
+    .addFontDictionary('GreatVibes-R', FontGreatVibesR)
+    .addFontDictionary('Fantasque-BI', FontFantasqueBI)
+    .addFontDictionary('AppleStorm-R', FontAppleStormR)
+    .addFontDictionary('Hussar3D-R', FontHussar3D)
+    .addContentStreams(page2ContentStreamRef);
+
+  const page3Width = 750;
+  const page3Height = 1750;
+  const page3ContentStream = makePage3ContentStream(pdfDoc, page3Height);
+  const page3ContentStreamRef = pdfDoc.register(page3ContentStream);
+
+  const page3 = pdfDoc
+    .createPage([page3Width, page3Height])
+    .addXObject('CatRidingUnicorn', JpgCatRidingUnicorn)
+    .addXObject('MinionsLaughing', JpgMinionsLaughing)
+    .addXObject('GreyscaleBird', PngGreyscaleBird)
+    .addXObject('MinionsBananaAlpha', PngMinionsBananaAlpha)
+    .addXObject('MinionsBananaNoAlpha', PngMinionsBananaNoAlpha)
+    .addXObject('SmallMario', PngSmallMario)
+    .addContentStreams(page3ContentStreamRef);
 
   // Add pages:
   pdfDoc.addPage(page1);
-  // pdfDoc.addPage(page2);
-  // pdfDoc.addPage(page3);
+  pdfDoc.addPage(page2);
+  pdfDoc.addPage(page3);
 
   return PDFDocumentWriter.saveToBytes(pdfDoc);
 };
