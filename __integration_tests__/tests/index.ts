79aaa576c818cf32e0126539fe8b08e806d15ead
Andrew Dillon <andrew.dillon.j@gmail.com>
2018-06-09 17:13:56 -0500

    Complete integration test5

--- __integration_tests__/tests/index.ts
+++ __integration_tests__/tests/index.ts
@@ -1,4 +1,5 @@
 // export { default as test1 } from './test1';
 // export { default as test2 } from './test2';
 // export { default as test3 } from './test3';
-export { default as test4 } from './test4';
+// export { default as test4 } from './test4';
+export { default as test5 } from './test5';
---
+++ __integration_tests__/tests/test5.ts
@@ -0,0 +1,150 @@
+import faker from 'faker';
+import fs from 'fs';
+import _ from 'lodash';
+
+import PDFDocument from 'core/pdf-document/PDFDocument';
+import PDFDocumentFactory from 'core/pdf-document/PDFDocumentFactory';
+import PDFDocumentWriter from 'core/pdf-document/PDFDocumentWriter';
+import Standard14Fonts from 'core/pdf-document/Standard14Fonts';
+import { PDFDictionary, PDFIndirectReference, PDFName } from 'core/pdf-objects';
+import PDFTextObject from 'core/pdf-operators/text/PDFTextObject';
+import { PDFContentStream } from 'core/pdf-structures';
+import PDFPage from 'core/pdf-structures/PDFPage';
+
+import {
+  drawCircle,
+  drawEllipse,
+  drawImage,
+  drawLinesOfText,
+  drawRectangle,
+  drawSquare,
+  drawText,
+} from 'core/pdf-operators/helpers/composite';
+import {
+  clip,
+  clipEvenOdd,
+  closePath,
+  dashPattern,
+  endPath,
+  fillingRgbColor,
+  fontAndSize,
+  lineCap,
+  lineHeight,
+  lineJoin,
+  lineTo,
+  moveTo,
+  nextLine,
+  popGraphicsState,
+  pushGraphicsState,
+  scale,
+  strokingRgbColor,
+  text,
+  textRenderingMode,
+  translate,
+} from 'core/pdf-operators/helpers/simple';
+
+import { IPDFCreator, ITest, ITestAssets } from '../models';
+
+const makeOverlayContentStream = (
+  pdfDoc: PDFDocument,
+  marioDims: { width: number; height: number },
+) =>
+  pdfDoc.createContentStream(
+    ...drawImage('Mario', {
+      x: 200,
+      y: 375,
+      width: marioDims.width * 0.15,
+      height: marioDims.height * 0.15,
+    }),
+    ...drawRectangle({
+      x: 120,
+      y: 265,
+      width: 400,
+      height: 90,
+      fillRgbColor: [253 / 255, 246 / 255, 227 / 255],
+      borderWidth: 3,
+      borderRgbColor: [101 / 255, 123 / 255, 131 / 255],
+    }),
+    ...drawLinesOfText(
+      [
+        'This is an image of Mario running.',
+        'This image and text was drawn on',
+        'top of an existing PDF using pdf-lib!',
+      ],
+      {
+        x: 125,
+        y: 325,
+        fillRgbColor: [101 / 255, 123 / 255, 131 / 255],
+        font: 'Ubuntu',
+        size: 24,
+      },
+    ),
+  );
+
+const makeMiddlePageContentStream = (pdfDoc: PDFDocument, pageHeight: number) =>
+  pdfDoc.createContentStream(
+    ..._.flatMap(Standard14Fonts, (font, idx) =>
+      drawText(`${idx + 1}. These are the 14 Standard Fonts.`, {
+        x: 5,
+        y: pageHeight - (idx + 1) * 20,
+        size: 20,
+        font,
+      }),
+    ),
+  );
+
+// Define the test kernel using the above content stream functions.
+const kernel: IPDFCreator = (assets: ITestAssets) => {
+  const pdfDoc = PDFDocumentFactory.load(assets.pdfs.normal);
+
+  const [FontTimesRoman] = pdfDoc.embedStandardFont('Times-Roman');
+  const [FontUbuntu] = pdfDoc.embedFont(assets.fonts.ttf.ubuntu_r);
+
+  const [PngMario, marioDims] = pdfDoc.embedPNG(assets.images.png.small_mario);
+
+  const pages = pdfDoc.getPages();
+
+  const overlayContentStreamRef = pdfDoc.register(
+    makeOverlayContentStream(pdfDoc, marioDims),
+  );
+
+  pages.forEach((page) => {
+    page
+      .addFontDictionary('Times-Roman', FontTimesRoman)
+      .addFontDictionary('Ubuntu', FontUbuntu)
+      .addXObject('Mario', PngMario)
+      .addContentStreams(overlayContentStreamRef);
+  });
+
+  const middlePageContentStreamRef = pdfDoc.register(
+    makeMiddlePageContentStream(pdfDoc, 500),
+  );
+
+  const middlePage = pdfDoc
+    .createPage([600, 500])
+    .addContentStreams(middlePageContentStreamRef);
+
+  Standard14Fonts.forEach((font) => {
+    const [fontRef] = pdfDoc.embedStandardFont(font);
+    middlePage.addFontDictionary(font, fontRef);
+  });
+
+  pdfDoc.insertPage(1, middlePage);
+
+  return PDFDocumentWriter.saveToBytes(pdfDoc);
+};
+
+export default {
+  kernel,
+  title: 'Basic PDF Modification Test',
+  description: 'This tests that basic PDFs can be modified.',
+  checklist: [
+    'the document has 3 pages.',
+    'the first and last pages constitute a "2012 D-2210" tax form.',
+    'the first and last pages contain a picture of Mario.',
+    'the first and last pages contains a box of solarized text beneath Mario.',
+    'the middle page has 14 lines of text that say "These are the 14 Standard Fonts."',
+    'each line of text is in a different font.',
+    'the 4th and 8th fonts are symbolic non-latin fonts.',
+  ],
+};
