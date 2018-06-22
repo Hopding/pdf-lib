fa0d2b49844e0e9f963184789f742109ba8a223f
Andrew Dillon <tajd1@unigroupinc.com>
2018-06-21 21:49:57 -0500

    Compile minified UMD builds

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,7 @@ const filter = require('gulp-filter');
 const del = require('del');
 const { execSync } = require('child_process');
 
-/*
-  compiled/dist
-    - Single UMD file - unminified
-    - Single UMD file - minified
-  compiled/es
-    - Single ES6 file - unminified
-  compiled/lib
-    - Single CommonJS file - unminified
-  compiled/src
-    - Set of ES6 source files - unminified
-*/
+/* =============================== Setup ==================================== */
 
 const tsProject = ts.createProject('tsconfig.json', { module: 'es2015' });
 
@@ -35,6 +25,19 @@ const babelrc = {
   ],
 };
 
+const jsFilter = filter(['**/*.js'], { restore: true });
+
+const rollup = (dir, format, minify) => () =>
+  execSync(`
+    export UGLIFY=${minify};        \\
+    yarn rollup                     \\
+      -c rollup.config.js           \\
+      -o compiled/${dir}/pdf-lib${minify ? '.min' : ''}.js \\
+      --format ${format}
+  `);
+
+/* =============================== Tasks ==================================== */
+
 gulp.task('prepare-release', () =>
   execSync(`
     rm -rf node_modules && \\
@@ -44,24 +47,22 @@ gulp.task('prepare-release', () =>
   `),
 );
 
-gulp.task('build-release', ['rollup-commonjs', 'rollup-es', 'rollup-umd'], () =>
-  execSync(`
+gulp.task(
+  'build-release',
+  ['rollup-commonjs', 'rollup-es', 'rollup-umd', 'rollup-umd-min'],
+  () =>
+    execSync(`
     cp -r  package.json README.md LICENSE.md compiled/ && \\
     rm -rf compiled/__integration_tests__ compiled/src
   `),
 );
 
-gulp.task('build-dev', ['rollup-commonjs', 'rollup-es', 'rollup-umd']);
-
-const jsFilter = filter(['**/*.js'], { restore: true });
-
-const rollup = (dir, format) => () =>
-  execSync(`
-    yarn rollup                     \\
-      -c rollup.config.js           \\
-      -o compiled/${dir}/pdf-lib.js \\
-      --format ${format}
-  `);
+gulp.task('build-dev', [
+  'rollup-commonjs',
+  'rollup-es',
+  'rollup-umd',
+  'rollup-umd-min',
+]);
 
 gulp.task('clean', () => del('compiled/**'));
 
@@ -78,3 +79,4 @@ gulp.task('compile-to-js', ['clean'], () =>
 gulp.task('rollup-commonjs', ['compile-to-js'], rollup('lib', 'cjs'));
 gulp.task('rollup-es', ['compile-to-js'], rollup('es', 'es'));
 gulp.task('rollup-umd', ['compile-to-js'], rollup('dist', 'umd'));
+gulp.task('rollup-umd-min', ['compile-to-js'], rollup('dist', 'umd', true));
--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+import { plugin as analyze } from 'rollup-plugin-analyzer';
+
+const { UGLIFY } = process.env;
 
 export default {
   input: 'compiled/src/index.js',
@@ -7,6 +11,7 @@ export default {
     name: 'PDFLib',
   },
   plugins: [
+    analyze(),
     nodeResolve({
       jsnext: true,
     }),
@@ -15,5 +20,6 @@ export default {
         'node_modules/lodash/index.js': ['default'],
       },
     }),
+    UGLIFY === 'true' && uglify(),
   ],
 };
