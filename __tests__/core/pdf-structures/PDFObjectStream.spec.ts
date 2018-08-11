ef9abe6eeb8da68aa841b223b4dd4610080c23a2
Andrew Dillon <andrew.dillon.j@gmail.com>
2018-08-11 16:17:45 -0500

    Add unit test for PDFObjectStream class

--- __tests__/core/pdf-structures/PDFObjectStream.spec.ts
+++ __tests__/core/pdf-structures/PDFObjectStream.spec.ts
@@ -1,12 +1,15 @@
 import PDFObjectIndex from 'core/pdf-document/PDFObjectIndex';
 import {
+  PDFArray,
   PDFDictionary,
   PDFIndirectObject,
+  PDFNumber,
   PDFObject,
   PDFString,
 } from 'core/pdf-objects';
+import PDFOperators from 'core/pdf-operators';
 import { PDFObjectStream } from 'core/pdf-structures';
-import { typedArrayFor } from 'utils';
+import { arrayToString, typedArrayFor } from 'utils';
 
 describe(`PDFObjectStream`, () => {
   it(`extends PDFObject`, () => {
@@ -64,6 +67,78 @@ describe(`PDFObjectStream`, () => {
           PDFIndirectObject.of(PDFString.fromString('bar')),
         ]),
       ).toBeInstanceOf(PDFObjectStream);
+    });
+  });
+
+  describe(`"bytesSize" method`, () => {
+    it(`returns the size of the PDFObjectStream in bytes`, () => {
+      const index = PDFObjectIndex.create();
+
+      const object1 = PDFString.fromString('FooBar');
+      const object2 = PDFDictionary.from(
+        { Qux: PDFString.fromString('Baz') },
+        index,
+      );
+      const object3 = PDFArray.fromArray(
+        [
+          PDFDictionary.from({ Qux: PDFString.fromString('Baz') }, index),
+          PDFNumber.fromNumber(21),
+        ],
+        index,
+      );
+
+      const objStream = PDFObjectStream.create(index, [
+        PDFIndirectObject.of(object1).setReferenceNumbers(1, 0),
+        PDFIndirectObject.of(object2).setReferenceNumbers(2, 0),
+        PDFIndirectObject.of(object3).setReferenceNumbers(9000, 0),
+      ]);
+
+      expect(objStream.bytesSize()).toBe(125);
+    });
+  });
+
+  describe(`"copyBytesInto" method`, () => {
+    it(`copies the PDFObjectStream into the buffer as bytes`, () => {
+      const index = PDFObjectIndex.create();
+
+      const object1 = PDFString.fromString('FooBar');
+      const object2 = PDFDictionary.from(
+        { Qux: PDFString.fromString('Baz') },
+        index,
+      );
+      const object3 = PDFArray.fromArray(
+        [
+          PDFDictionary.from({ Qux: PDFString.fromString('Baz') }, index),
+          PDFNumber.fromNumber(21),
+        ],
+        index,
+      );
+
+      const objStream = PDFObjectStream.create(index, [
+        PDFIndirectObject.of(object1).setReferenceNumbers(1, 0),
+        PDFIndirectObject.of(object2).setReferenceNumbers(2, 0),
+        PDFIndirectObject.of(object3).setReferenceNumbers(9000, 0),
+      ]);
+
+      const buffer = new Uint8Array(objStream.bytesSize());
+      objStream.copyBytesInto(buffer);
+
+      const expected = `
+<<
+/Type /ObjStm
+/Length 62
+/N 3
+/First 15
+>>
+stream
+1 0 2 8 9000 24(FooBar)<<
+/Qux (Baz)
+>>[ <<
+/Qux (Baz)
+>> 21 ]
+endstream
+  `.trim();
+      expect(buffer).toEqual(typedArrayFor(expected));
     });
   });
 });
--- src/core/pdf-structures/PDFObjectStream.ts
+++ src/core/pdf-structures/PDFObjectStream.ts
@@ -1,7 +1,7 @@
 import add from 'lodash/add';
+import dropRight from 'lodash/dropRight';
 import flatten from 'lodash/flatten';
 import last from 'lodash/last';
-import tail from 'lodash/tail';
 
 import { PDFObjectIndex } from 'core/pdf-document';
 import {
@@ -37,41 +37,6 @@ class PDFObjectStream extends PDFStream {
     this.objects = objects;
   }
 
-  objectByteOffsets = (): number[] =>
-    tail(this.objects).reduce(
-      (offsets, obj) => offsets.concat(last(offsets)! + obj.bytesSize()),
-      [0],
-    );
-
-  leadingIntegerPairs = (): Array<[number, number]> => {
-    const byteOffsets = this.objectByteOffsets();
-    return this.objects.map(
-      (obj, idx) =>
-        [obj.reference.objectNumber, byteOffsets[idx]] as [number, number],
-    );
-  };
-
-  updateDictionary = () => {
-    this.dictionary.set(
-      PDFName.from('Length'),
-      PDFNumber.fromNumber(this.contentBytesSize()),
-    );
-    this.dictionary.set(
-      PDFName.from('N'),
-      PDFNumber.fromNumber(this.objects.length),
-    );
-    this.dictionary.set(
-      PDFName.from('First'),
-      PDFNumber.fromNumber(
-        flatten(this.leadingIntegerPairs()).join(' ').length,
-      ),
-    );
-  };
-
-  contentBytesSize = (): number =>
-    flatten(this.leadingIntegerPairs()).join(' ').length +
-    this.objects.map((obj) => obj.bytesSize()).reduce(add, 0);
-
   bytesSize = (): number => {
     this.updateDictionary();
     return (
@@ -89,10 +54,7 @@ class PDFObjectStream extends PDFStream {
 
     let remaining = this.dictionary.copyBytesInto(buffer);
     remaining = addStringToBuffer('\nstream\n', remaining);
-    remaining = addStringToBuffer(
-      flatten(this.leadingIntegerPairs()).join(' '),
-      remaining,
-    );
+    remaining = addStringToBuffer(this.leadingIntegerPairsStr(), remaining);
 
     remaining = this.objects.reduce(
       (remBytes: Uint8Array, obj: PDFIndirectObject) =>
@@ -102,6 +64,43 @@ class PDFObjectStream extends PDFStream {
 
     remaining = addStringToBuffer('\nendstream', remaining);
     return remaining;
+  };
+
+  private contentBytesSize = (): number =>
+    this.leadingIntegerPairsStr().length +
+    this.objects.map((obj) => obj.pdfObject.bytesSize()).reduce(add, 0);
+
+  private objectByteOffsets = (): number[] =>
+    dropRight(this.objects).reduce(
+      (offsets, obj) =>
+        offsets.concat(last(offsets)! + obj.pdfObject.bytesSize()),
+      [0],
+    );
+
+  private leadingIntegerPairs = (): Array<[number, number]> => {
+    const byteOffsets = this.objectByteOffsets();
+    return this.objects.map(
+      (obj, idx) =>
+        [obj.reference.objectNumber, byteOffsets[idx]] as [number, number],
+    );
+  };
+
+  private leadingIntegerPairsStr = (): string =>
+    flatten(this.leadingIntegerPairs()).join(' ');
+
+  private updateDictionary = () => {
+    this.dictionary.set(
+      PDFName.from('Length'),
+      PDFNumber.fromNumber(this.contentBytesSize()),
+    );
+    this.dictionary.set(
+      PDFName.from('N'),
+      PDFNumber.fromNumber(this.objects.length),
+    );
+    this.dictionary.set(
+      PDFName.from('First'),
+      PDFNumber.fromNumber(this.leadingIntegerPairsStr().length),
+    );
   };
 }
 
