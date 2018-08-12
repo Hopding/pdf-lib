55c4774e11fa16ac6e92a26e48472dc6e7edc17f
Andrew Dillon <andrew.dillon.j@gmail.com>
2018-08-12 18:20:58 -0500

    Messy (working) implementation of serialization with object streams

--- src/core/pdf-document/PDFDocumentWriter.ts
+++ src/core/pdf-document/PDFDocumentWriter.ts
@@ -1,5 +1,11 @@
+import flatMap from 'lodash/flatMap';
+import isNil from 'lodash/isNil';
+import last from 'lodash/last';
+import sortBy from 'lodash/sortBy';
+
 import PDFDocument from 'core/pdf-document/PDFDocument';
 import {
+  PDFArray,
   PDFDictionary,
   PDFIndirectObject,
   PDFIndirectReference,
@@ -112,23 +118,81 @@ class PDFDocumentWriter {
     xrefStream.dictionary.set(PDFName.from('Root'), pdfCatalogRef!);
     xrefStream.addFreeObjectEntry(0, 65535);
 
+    interface IGroupElement {
+      objectNumber: number;
+      generationNumber: number;
+      offset?: number;
+      index?: number;
+    }
+    const group: IGroupElement[] = [];
+
     objectStream.objects.forEach((obj, idx) => {
-      xrefStream.addCompressedObjectEntry(objectStreamRef.objectNumber, idx);
+      // xrefStream.addCompressedObjectEntry(objectStreamRef.objectNumber, idx);
+      group.push({
+        objectNumber: obj.reference.objectNumber,
+        generationNumber: obj.reference.generationNumber,
+        index: idx,
+      });
     });
 
     let offset = pdfDoc.header.bytesSize();
     streamObjects.forEach((obj) => {
       const { generationNumber } = obj.reference;
-      xrefStream.addUncompressedObjectEntry(offset, generationNumber);
+      // xrefStream.addUncompressedObjectEntry(offset, generationNumber);
+      group.push({
+        objectNumber: obj.reference.objectNumber,
+        generationNumber: obj.reference.generationNumber,
+        offset,
+      });
       offset += obj.bytesSize();
     });
 
     const trailer = PDFTrailerX.from(offset);
 
-    xrefStream.addUncompressedObjectEntry(
+    /* ===== Add Index... ===== */
+    group.push({
+      objectNumber: xrefStreamRef.objectNumber,
+      generationNumber: xrefStreamRef.generationNumber,
       offset,
-      xrefStreamRef.generationNumber,
+    });
+
+    const sortedGroup: IGroupElement[] = sortBy(group, 'objectNumber');
+    const groupChunks: Array<{ firstObjectNumber: number; size: number }> = [
+      { firstObjectNumber: 0, size: 1 },
+    ];
+
+    sortedGroup.forEach((obj, idx) => {
+      const shouldStartNewSection =
+        idx !== 0 && obj.objectNumber - sortedGroup[idx - 1].objectNumber > 1;
+
+      if (shouldStartNewSection) {
+        groupChunks.push({ firstObjectNumber: obj.objectNumber, size: 1 });
+      } else {
+        last(groupChunks)!.size += 1;
+      }
+
+      if (!isNil(obj.offset)) {
+        xrefStream.addUncompressedObjectEntry(obj.offset, obj.generationNumber);
+      }
+      if (!isNil(obj.index)) {
+        xrefStream.addCompressedObjectEntry(
+          objectStreamRef.objectNumber,
+          obj.index,
+        );
+      }
+    });
+
+    const index = PDFArray.fromArray(
+      flatMap(groupChunks, ({ firstObjectNumber, size }) => [
+        PDFNumber.fromNumber(firstObjectNumber),
+        PDFNumber.fromNumber(size),
+      ]),
+      pdfDoc.index,
     );
+
+    xrefStream.dictionary.set(PDFName.from('Index'), index);
+    /* ======================== */
+
     offset += xrefStreamIndObj.bytesSize();
 
     /* ----- */
