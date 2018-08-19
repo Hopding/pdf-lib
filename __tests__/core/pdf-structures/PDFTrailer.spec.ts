5006f35b68e5c11ff987213169f7fac94a6ab0c2
Andrew Dillon <andrew.dillon.j@gmail.com>
2018-08-19 16:01:33 -0500

    Update PDFTrailer unit tests

--- __tests__/core/pdf-structures/PDFTrailer.spec.ts
+++ __tests__/core/pdf-structures/PDFTrailer.spec.ts
@@ -13,10 +13,10 @@ describe(`PDFTrailer`, () => {
   it(`requires a Number and PDFDictionary to be constructed`, () => {
     expect(() => new PDFTrailer()).toThrowError();
     expect(() => new PDFTrailer([])).toThrowError(
-      'PDFTrailer offsets can only be Numbers',
+      'PDFTrailer.offset must be a number',
     );
-    expect(() => new PDFTrailer(21)).toThrowError(
-      'PDFTrailer dictionaries can only be PDFDictionaries',
+    expect(() => new PDFTrailer(21, {})).toThrowError(
+      'PDFTrailer.dictionary must be instance of PDFDictionary or undefined',
     );
     expect(
       new PDFTrailer(21, PDFDictionary.from({}, PDFObjectIndex.create())),
@@ -26,10 +26,10 @@ describe(`PDFTrailer`, () => {
   it(`has a static "from" factory method`, () => {
     expect(() => PDFTrailer.from()).toThrowError();
     expect(() => PDFTrailer.from([])).toThrowError(
-      'PDFTrailer offsets can only be Numbers',
+      'PDFTrailer.offset must be a number',
     );
-    expect(() => PDFTrailer.from(21)).toThrowError(
-      'PDFTrailer dictionaries can only be PDFDictionaries',
+    expect(() => PDFTrailer.from(21, {})).toThrowError(
+      'PDFTrailer.dictionary must be instance of PDFDictionary or undefined',
     );
     expect(
       PDFTrailer.from(21, PDFDictionary.from({}, PDFObjectIndex.create())),
@@ -37,7 +37,12 @@ describe(`PDFTrailer`, () => {
   });
 
   describe(`"toString" method`, () => {
-    it(`returns the PDFTrailer as a string`, () => {
+    it(`returns the PDFTrailer without a dictionary as a string`, () => {
+      const pdfTrailer = PDFTrailer.from(2921);
+      expect(pdfTrailer.toString()).toBe(`startxref\n` + `2921\n` + `%%EOF\n`);
+    });
+
+    it(`returns the PDFTrailer with a dictionary as a string`, () => {
       const pdfTrailer = PDFTrailer.from(
         2921,
         PDFDictionary.from(
@@ -56,7 +61,12 @@ describe(`PDFTrailer`, () => {
   });
 
   describe(`"bytesSize" method`, () => {
-    it(`returns the size of the PDFTrailer in bytes`, () => {
+    it(`returns the size of the PDFTrailer without a dictionary in bytes`, () => {
+      const pdfTrailer = PDFTrailer.from(2921);
+      expect(pdfTrailer.bytesSize()).toBe(21);
+    });
+
+    it(`returns the size of the PDFTrailer with a dictionary in bytes`, () => {
       const pdfTrailer = PDFTrailer.from(
         2921,
         PDFDictionary.from(
@@ -69,7 +79,18 @@ describe(`PDFTrailer`, () => {
   });
 
   describe(`"copyBytesInto" method`, () => {
-    it(`copies the PDFTrailer into the buffer as bytes`, () => {
+    it(`copies the PDFTrailer without a dictionary into the buffer as bytes`, () => {
+      const pdfTrailer = PDFTrailer.from(2921);
+
+      const bytes = new Uint8Array(pdfTrailer.bytesSize());
+      pdfTrailer.copyBytesInto(bytes);
+
+      expect(bytes).toEqual(
+        typedArrayFor(`startxref\n` + `2921\n` + `%%EOF\n`),
+      );
+    });
+
+    it(`copies the PDFTrailer with a dictionary into the buffer as bytes`, () => {
       const pdfTrailer = PDFTrailer.from(
         2921,
         PDFDictionary.from(
