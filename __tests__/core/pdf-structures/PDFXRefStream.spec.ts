3c33ad6d1bd082945391da5f6f0cf30401d68e97
Andrew Dillon <tajd1@unigroupinc.com>
2018-08-20 17:57:53 -0500

    Update tests for PDFXRefStream

--- __tests__/core/pdf-structures/PDFXRefStream.spec.ts
+++ __tests__/core/pdf-structures/PDFXRefStream.spec.ts
@@ -1,45 +1,74 @@
+import pako from 'pako';
+
 // Required to prevent an issue with circular dependency resolution in this test
 import 'core/pdf-objects';
 
 import PDFObjectIndex from 'core/pdf-document/PDFObjectIndex';
-import { PDFDictionary, PDFNumber, PDFStream } from 'core/pdf-objects';
+import {
+  PDFDictionary,
+  PDFIndirectReference,
+  PDFNumber,
+  PDFStream,
+} from 'core/pdf-objects';
 import PDFOperators from 'core/pdf-operators';
 import { PDFXRefStream } from 'core/pdf-structures';
-import { arrayToString, typedArrayFor } from 'utils';
+import { arrayToString, mergeUint8Arrays, typedArrayFor } from 'utils';
 
 describe(`PDFXRefStream`, () => {
   it(`inherits from PDFSteam`, () => {
     expect(
-      new PDFXRefStream(PDFDictionary.from({}, PDFObjectIndex.create())),
+      new PDFXRefStream(
+        {
+          Size: PDFNumber.fromNumber(21),
+          Root: PDFIndirectReference.forNumbers(2, 0),
+        },
+        PDFObjectIndex.create(),
+      ),
     ).toBeInstanceOf(PDFStream);
   });
 
   describe(`static "create" factory method`, () => {
     it(`requires a PDFObjectIndex to be constructed`, () => {
-      expect(() => PDFXRefStream.create()).toThrowError(
+      const sizeAndRoot = {
+        Size: PDFNumber.fromNumber(21),
+        Root: PDFIndirectReference.forNumbers(2, 0),
+      };
+      expect(() => PDFXRefStream.create(sizeAndRoot)).toThrowError(
         '"index" must be an instance of PDFObjectIndex',
       );
-      expect(PDFXRefStream.create(PDFObjectIndex.create())).toBeInstanceOf(
-        PDFXRefStream,
-      );
+      expect(
+        PDFXRefStream.create(sizeAndRoot, PDFObjectIndex.create()),
+      ).toBeInstanceOf(PDFXRefStream);
     });
   });
 
   describe(`"bytesSize" method`, () => {
     it(`returns the size of the PDFXRefStream in bytes`, () => {
-      const xrefStream = PDFXRefStream.create(PDFObjectIndex.create());
+      const xrefStream = PDFXRefStream.create(
+        {
+          Size: PDFNumber.fromNumber(21),
+          Root: PDFIndirectReference.forNumbers(2, 0),
+        },
+        PDFObjectIndex.create(),
+      );
 
       xrefStream.addFreeObjectEntry(11, 2);
       xrefStream.addUncompressedObjectEntry(30, 40);
       xrefStream.addCompressedObjectEntry(5, 691);
 
-      expect(xrefStream.bytesSize()).toBe(85);
+      expect(xrefStream.bytesSize()).toBe(92);
     });
   });
 
   describe(`"copyBytesInto" method`, () => {
     it(`copies the PDFXRefStream into the buffer as bytes`, () => {
-      const xrefStream = PDFXRefStream.create(PDFObjectIndex.create());
+      const xrefStream = PDFXRefStream.create(
+        {
+          Size: PDFNumber.fromNumber(21),
+          Root: PDFIndirectReference.forNumbers(2, 0),
+        },
+        PDFObjectIndex.create(),
+      );
 
       xrefStream.addFreeObjectEntry(11, 2);
       xrefStream.addUncompressedObjectEntry(30, 40);
@@ -48,19 +77,75 @@ describe(`PDFXRefStream`, () => {
       const buffer = new Uint8Array(xrefStream.bytesSize());
       xrefStream.copyBytesInto(buffer);
 
-      const expected = `
-<<
+      // prettier-ignore
+      const expectedEntries = new Uint8Array([
+        0, 11, 0, 2,
+        1, 30, 0, 40,
+        2,  5, 2, 691
+      ]);
+
+      // prettier-ignore
+      const expected = mergeUint8Arrays(
+          typedArrayFor(
+`<<
 /Type /XRef
-/W [ 1 2 3 ]
-/Length 26
+/Size 21
+/Root 2 0 R
+/W [ 1 1 2 ]
+/Length 12
 >>
 stream
-0 11 002
-1 30 040
-2 05 691
-endstream
-  `.trim();
-      expect(buffer).toEqual(typedArrayFor(expected));
+`),
+          expectedEntries,
+          typedArrayFor(`\nendstream`),
+      );
+
+      expect(buffer).toEqual(expected);
+    });
+
+    it(`copies the encoded PDFXRefStream into the buffer as bytes`, () => {
+      const xrefStream = PDFXRefStream.create(
+        {
+          Size: PDFNumber.fromNumber(21),
+          Root: PDFIndirectReference.forNumbers(2, 0),
+        },
+        PDFObjectIndex.create(),
+      );
+
+      xrefStream.addFreeObjectEntry(11, 2);
+      xrefStream.addUncompressedObjectEntry(30, 40);
+      xrefStream.addCompressedObjectEntry(5, 691);
+
+      xrefStream.encode();
+
+      const buffer = new Uint8Array(xrefStream.bytesSize());
+      xrefStream.copyBytesInto(buffer);
+
+      // prettier-ignore
+      const expectedEntries = pako.deflate(new Uint8Array([
+        0, 11, 0, 2,
+        1, 30, 0, 40,
+        2,  5, 2, 691
+      ]));
+
+      // prettier-ignore
+      const expected = mergeUint8Arrays(
+          typedArrayFor(
+`<<
+/Type /XRef
+/Size 21
+/Root 2 0 R
+/Filter /FlateDecode
+/W [ 1 1 2 ]
+/Length 20
+>>
+stream
+`),
+          expectedEntries,
+          typedArrayFor(`\nendstream`),
+      );
+
+      expect(buffer).toEqual(expected);
     });
   });
 });
