cb16e565dcf44326d301fdddd94a651c662aeafb
Andrew Dillon <andrew_dillon@unigroup.com>
2019-05-09 15:35:17 -0500

    Add PDFRef

---
+++ src/core/objects/PDFRef.ts
@@ -0,0 +1,56 @@
+import { PrivateConstructorError } from 'src/core/errors';
+import PDFObject from 'src/core/objects/PDFObject';
+
+const ENFORCER = {};
+const pool = new Map<string, PDFRef>();
+
+class PDFRef extends PDFObject {
+  static of = (objectNumber: number, generationNumber = 0) => {
+    const tag = `${objectNumber} ${generationNumber} R`;
+
+    let instance = pool.get(tag);
+    if (!instance) {
+      instance = new PDFRef(ENFORCER, objectNumber, generationNumber);
+      pool.set(tag, instance);
+    }
+
+    return instance;
+  };
+
+  readonly objectNumber: number;
+  readonly generationNumber: number;
+  readonly tag: string;
+
+  private constructor(
+    enforcer: any,
+    objectNumber: number,
+    generationNumber: number,
+  ) {
+    if (enforcer !== ENFORCER) throw new PrivateConstructorError('PDFRef');
+    super();
+    this.objectNumber = objectNumber;
+    this.generationNumber = generationNumber;
+    this.tag = `${objectNumber} ${generationNumber} R`;
+  }
+
+  clone(): PDFRef {
+    return this;
+  }
+
+  toString(): string {
+    return this.tag;
+  }
+
+  sizeInBytes(): number {
+    return this.tag.length;
+  }
+
+  copyBytesInto(buffer: Uint8Array, offset: number): number {
+    for (let idx = 0, len = this.tag.length; idx < len; idx++) {
+      buffer[offset++] = this.tag.charCodeAt(idx);
+    }
+    return this.tag.length;
+  }
+}
+
+export default PDFRef;
