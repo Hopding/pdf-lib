628de71fa3ddba86618b91e2976091ba57d76149
Andrew Dillon <andrew_dillon@unigroup.com>
2019-05-10 18:44:00 -0500

    Add PDFTrailer and PDFTrailerDict

---
+++ src/core/document/PDFTrailer.ts
@@ -0,0 +1,50 @@
+import { CharCodes } from '../enums';
+
+class PDFTrailer {
+  static forLastCrossRefSectionOffset = (offset: number) =>
+    new PDFTrailer(offset);
+
+  private readonly lastXRefOffset: string;
+
+  private constructor(lastXRefOffset: number) {
+    this.lastXRefOffset = String(lastXRefOffset);
+  }
+
+  toString(): string {
+    return `startxref\n${this.lastXRefOffset}\n%%EOF`;
+  }
+
+  sizeInBytes(): number {
+    return 16 + this.lastXRefOffset.length;
+  }
+
+  copyBytesInto(buffer: Uint8Array, offset: number): number {
+    const initialOffset = offset;
+
+    buffer[offset++] = CharCodes.s;
+    buffer[offset++] = CharCodes.t;
+    buffer[offset++] = CharCodes.a;
+    buffer[offset++] = CharCodes.r;
+    buffer[offset++] = CharCodes.t;
+    buffer[offset++] = CharCodes.x;
+    buffer[offset++] = CharCodes.r;
+    buffer[offset++] = CharCodes.e;
+    buffer[offset++] = CharCodes.f;
+    buffer[offset++] = CharCodes.Newline;
+
+    for (let idx = 0, len = this.lastXRefOffset.length; idx < len; idx++) {
+      buffer[offset++] = this.lastXRefOffset.charCodeAt(idx);
+    }
+
+    buffer[offset++] = CharCodes.Newline;
+    buffer[offset++] = CharCodes.Percent;
+    buffer[offset++] = CharCodes.Percent;
+    buffer[offset++] = CharCodes.E;
+    buffer[offset++] = CharCodes.O;
+    buffer[offset++] = CharCodes.F;
+
+    return offset - initialOffset;
+  }
+}
+
+export default PDFTrailer;
---
+++ src/core/document/PDFTrailerDict.ts
@@ -0,0 +1,38 @@
+import PDFDict from 'src/core/objects/PDFDict';
+import { CharCodes } from '../enums';
+
+class PDFTrailerDict {
+  static of = (dict: PDFDict) => new PDFTrailerDict(dict);
+  private readonly dict: PDFDict;
+
+  private constructor(dict: PDFDict) {
+    this.dict = dict;
+  }
+
+  toString(): string {
+    return `trailer\n${this.dict.toString()}`;
+  }
+
+  sizeInBytes(): number {
+    return 8 + this.dict.sizeInBytes();
+  }
+
+  copyBytesInto(buffer: Uint8Array, offset: number): number {
+    const initialOffset = offset;
+
+    buffer[offset++] = CharCodes.t;
+    buffer[offset++] = CharCodes.r;
+    buffer[offset++] = CharCodes.a;
+    buffer[offset++] = CharCodes.i;
+    buffer[offset++] = CharCodes.l;
+    buffer[offset++] = CharCodes.e;
+    buffer[offset++] = CharCodes.r;
+    buffer[offset++] = CharCodes.Newline;
+
+    offset += this.dict.copyBytesInto(buffer, offset);
+
+    return offset - initialOffset;
+  }
+}
+
+export default PDFTrailerDict;
