cf10fd6938a70ee35a07e263c35b93896f0ec73f
Andrew Dillon <andrew_dillon@unigroup.com>
2019-05-10 20:33:14 -0500

    Add PDFCrossRefSection

---
+++ src/core/document/PDFCrossRefSection.ts
@@ -0,0 +1,146 @@
+import { padStart } from 'src/utils';
+import { PDFRef } from '..';
+import { CharCodes } from '../enums';
+
+export interface Entry {
+  ref: PDFRef;
+  offset: number;
+  deleted: boolean;
+}
+
+class PDFCrossRefSection {
+  static create = () => new PDFCrossRefSection();
+
+  private subsections: Entry[][] = [
+    [{ ref: PDFRef.of(0, 65535), offset: 0, deleted: true }],
+  ];
+  private chunkIdx = 0;
+  private chunkLength = 1;
+
+  addEntry(ref: PDFRef, offset: number): void {
+    this.append({ ref, offset, deleted: false });
+  }
+
+  addDeletedEntry(ref: PDFRef, nextFreeObjectNumber: number): void {
+    this.append({ ref, offset: nextFreeObjectNumber, deleted: true });
+  }
+
+  toString(): string {
+    let section = `xref\n`;
+
+    for (
+      let rangeIdx = 0, rangeLen = this.subsections.length;
+      rangeIdx < rangeLen;
+      rangeIdx++
+    ) {
+      const range = this.subsections[rangeIdx];
+      section += `${range[0].ref.objectNumber} ${range.length}\n`;
+      for (
+        let entryIdx = 0, entryLen = range.length;
+        entryIdx < entryLen;
+        entryIdx++
+      ) {
+        const entry = range[entryIdx];
+        section += padStart(String(entry.offset), 10, '0');
+        section += ' ';
+        section += padStart(String(entry.ref.generationNumber), 5, '0');
+        section += ' ';
+        section += entry.deleted ? 'f' : 'n';
+        section += ' \n';
+      }
+    }
+
+    return section;
+  }
+
+  sizeInBytes(): number {
+    let size = 5;
+    for (let idx = 0, len = this.subsections.length; idx < len; idx++) {
+      const subsection = this.subsections[idx];
+      const subsectionLength = subsection.length;
+      const [firstEntry] = subsection;
+      size += 2;
+      size += String(firstEntry.ref.objectNumber).length;
+      size += String(subsectionLength).length;
+      size += 20 * subsectionLength;
+    }
+    return size;
+  }
+
+  copyBytesInto(buffer: Uint8Array, offset: number): number {
+    const initialOffset = offset;
+
+    buffer[offset++] = CharCodes.x;
+    buffer[offset++] = CharCodes.r;
+    buffer[offset++] = CharCodes.e;
+    buffer[offset++] = CharCodes.f;
+    buffer[offset++] = CharCodes.Newline;
+
+    for (
+      let rangeIdx = 0, rangeLen = this.subsections.length;
+      rangeIdx < rangeLen;
+      rangeIdx++
+    ) {
+      const range = this.subsections[rangeIdx];
+
+      const firstObjectNumber = String(range[0].ref.objectNumber);
+      for (let idx = 0, len = firstObjectNumber.length; idx < len; idx++) {
+        buffer[offset++] = firstObjectNumber.charCodeAt(idx);
+      }
+      buffer[offset++] = CharCodes.Space;
+
+      const rangeLength = String(range.length);
+      for (let idx = 0, len = rangeLength.length; idx < len; idx++) {
+        buffer[offset++] = rangeLength.charCodeAt(idx);
+      }
+      buffer[offset++] = CharCodes.Newline;
+
+      for (
+        let entryIdx = 0, entryLen = range.length;
+        entryIdx < entryLen;
+        entryIdx++
+      ) {
+        const entry = range[entryIdx];
+
+        const entryOffset = padStart(String(entry.offset), 10, '0');
+        for (let idx = 0, len = entryOffset.length; idx < len; idx++) {
+          buffer[offset++] = entryOffset.charCodeAt(idx);
+        }
+        buffer[offset++] = CharCodes.Space;
+
+        const entryGen = padStart(String(entry.ref.generationNumber), 5, '0');
+        for (let idx = 0, len = entryGen.length; idx < len; idx++) {
+          buffer[offset++] = entryGen.charCodeAt(idx);
+        }
+        buffer[offset++] = CharCodes.Space;
+
+        if (entry.deleted) {
+          buffer[offset++] = CharCodes.f;
+        } else {
+          buffer[offset++] = CharCodes.n;
+        }
+
+        buffer[offset++] = CharCodes.Space;
+        buffer[offset++] = CharCodes.Newline;
+      }
+    }
+
+    return offset - initialOffset;
+  }
+
+  private append(currEntry: Entry): void {
+    const chunk = this.subsections[this.chunkIdx];
+    const prevEntry = chunk[this.chunkLength - 1];
+
+    if (currEntry.ref.objectNumber - prevEntry.ref.objectNumber > 1) {
+      this.subsections.push([currEntry]);
+      this.chunkIdx += 1;
+      this.chunkLength = 1;
+    } else {
+      chunk.push(currEntry);
+      this.chunkLength += 1;
+    }
+  }
+}
+
+export default PDFCrossRefSection;
---
+++ tests/core/document/PDFCrossRefSection.spec.ts
@@ -0,0 +1,88 @@
+import { PDFCrossRefSection, PDFRef } from 'src/core';
+import { toCharCode, typedArrayFor } from 'src/utils';
+
+describe(`PDFCrossRefSection`, () => {
+  it(`can be constructed from PDFCrossRefSection.create()`, () => {
+    expect(PDFCrossRefSection.create()).toBeInstanceOf(PDFCrossRefSection);
+  });
+
+  const xref1 = PDFCrossRefSection.create();
+  xref1.addEntry(PDFRef.of(1), 21);
+  xref1.addDeletedEntry(PDFRef.of(2, 1), 24);
+  xref1.addEntry(PDFRef.of(3), 192188923);
+  xref1.addEntry(PDFRef.of(4), 129219);
+
+  const xref2 = PDFCrossRefSection.create();
+  xref2.addEntry(PDFRef.of(3), 21);
+  xref2.addDeletedEntry(PDFRef.of(4, 1), 24);
+  xref2.addEntry(PDFRef.of(6), 192188923);
+  xref2.addEntry(PDFRef.of(7), 129219);
+
+  it(`can be converted to a string with a single subsection`, () => {
+    expect(String(xref1)).toEqual(
+      'xref\n' +
+        '0 5\n' +
+        '0000000000 65535 f \n' +
+        '0000000021 00000 n \n' +
+        '0000000024 00001 f \n' +
+        '0192188923 00000 n \n' +
+        '0000129219 00000 n \n',
+    );
+  });
+
+  it(`can be converted to a string with multiple subsections and without object number 1`, () => {
+    expect(String(xref2)).toEqual(
+      'xref\n' +
+        '0 1\n' +
+        '0000000000 65535 f \n' +
+        '3 2\n' +
+        '0000000021 00000 n \n' +
+        '0000000024 00001 f \n' +
+        '6 2\n' +
+        '0192188923 00000 n \n' +
+        '0000129219 00000 n \n',
+    );
+  });
+
+  it(`can provide its size in bytes with a single subsection`, () => {
+    expect(xref1.sizeInBytes()).toBe(109);
+  });
+
+  it(`can provide its size in bytes with multiple subsections and without object number 1`, () => {
+    expect(xref2.sizeInBytes()).toBe(117);
+  });
+
+  it(`can be serialized with a single subsection`, () => {
+    const buffer = new Uint8Array(113).fill(toCharCode(' '));
+    expect(xref1.copyBytesInto(buffer, 3)).toBe(109);
+    expect(buffer).toEqual(
+      typedArrayFor(
+        '   xref\n' +
+          '0 5\n' +
+          '0000000000 65535 f \n' +
+          '0000000021 00000 n \n' +
+          '0000000024 00001 f \n' +
+          '0192188923 00000 n \n' +
+          '0000129219 00000 n \n ',
+      ),
+    );
+  });
+
+  it(`can be serialized with multiple subsections and without object number 1`, () => {
+    const buffer = new Uint8Array(121).fill(toCharCode(' '));
+    expect(xref2.copyBytesInto(buffer, 3)).toBe(117);
+    expect(buffer).toEqual(
+      typedArrayFor(
+        '   xref\n' +
+          '0 1\n' +
+          '0000000000 65535 f \n' +
+          '3 2\n' +
+          '0000000021 00000 n \n' +
+          '0000000024 00001 f \n' +
+          '6 2\n' +
+          '0192188923 00000 n \n' +
+          '0000129219 00000 n \n ',
+      ),
+    );
+  });
+});
