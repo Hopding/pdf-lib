2661eae56517d063800c0a03078817f03936fca2
Andrew Dillon <andrew.dillon.j@gmail.com>
2019-05-22 17:50:39 -0500

    Convert LZWStream to TypeScript

--- src/core/streams/LZWStream.ts
+++ src/core/streams/LZWStream.ts
@@ -6,19 +6,24 @@
  * under the Apache 2.0 open source license.
  */
 
-const { DecodeStream } = require('src/core/streams/DecodeStream');
+import { DecodeStream } from 'src/core/streams/DecodeStream';
+import Stream from 'src/core/streams/Stream';
 
-/* tslint:disable */
-// prettier-ignore
-var LZWStream = (function LZWStreamClosure() {
-  function LZWStream(str, maybeLength, earlyChange) {
+class LZWStream extends (DecodeStream as any) {
+  constructor(
+    str: Stream,
+    maybeLength: number | undefined,
+    earlyChange: number,
+  ) {
+    super(maybeLength);
+
     this.str = str;
-    this.dict = str.dict;
+    // this.dict = str.dict;
     this.cachedData = 0;
     this.bitsCached = 0;
 
-    var maxLzwDictionarySize = 4096;
-    var lzwState = {
+    const maxLzwDictionarySize = 4096;
+    const lzwState = {
       earlyChange,
       codeLength: 9,
       nextCode: 258,
@@ -28,22 +33,18 @@ var LZWStream = (function LZWStreamClosure() {
       currentSequence: new Uint8Array(maxLzwDictionarySize),
       currentSequenceLength: 0,
     };
-    for (var i = 0; i < 256; ++i) {
+    for (let i = 0; i < 256; ++i) {
       lzwState.dictionaryValues[i] = i;
       lzwState.dictionaryLengths[i] = 1;
     }
     this.lzwState = lzwState;
-
-    DecodeStream.call(this, maybeLength);
   }
 
-  LZWStream.prototype = Object.create(DecodeStream.prototype);
-
-  LZWStream.prototype.readBits = function LZWStream_readBits(n) {
-    var bitsCached = this.bitsCached;
-    var cachedData = this.cachedData;
+  readBits(n: number) {
+    let bitsCached = this.bitsCached;
+    let cachedData = this.cachedData;
     while (bitsCached < n) {
-      var c = this.str.getByte();
+      const c = this.str.getByte();
       if (c === -1) {
         this.eof = true;
         return null;
@@ -51,40 +52,44 @@ var LZWStream = (function LZWStreamClosure() {
       cachedData = (cachedData << 8) | c;
       bitsCached += 8;
     }
-    this.bitsCached = (bitsCached -= n);
+    this.bitsCached = bitsCached -= n;
     this.cachedData = cachedData;
     this.lastCode = null;
     return (cachedData >>> bitsCached) & ((1 << n) - 1);
-  };
+  }
 
-  LZWStream.prototype.readBlock = function LZWStream_readBlock() {
-    var blockSize = 512;
-    var estimatedDecodedSize = blockSize * 2, decodedSizeDelta = blockSize;
-    var i, j, q;
+  readBlock() {
+    const blockSize = 512;
 
-    var lzwState = this.lzwState;
+    let estimatedDecodedSize = blockSize * 2;
+    const decodedSizeDelta = blockSize;
+    let i;
+    let j;
+    let q;
+
+    const lzwState = this.lzwState;
     if (!lzwState) {
       return; // eof was found
     }
 
-    var earlyChange = lzwState.earlyChange;
-    var nextCode = lzwState.nextCode;
-    var dictionaryValues = lzwState.dictionaryValues;
-    var dictionaryLengths = lzwState.dictionaryLengths;
-    var dictionaryPrevCodes = lzwState.dictionaryPrevCodes;
-    var codeLength = lzwState.codeLength;
-    var prevCode = lzwState.prevCode;
-    var currentSequence = lzwState.currentSequence;
-    var currentSequenceLength = lzwState.currentSequenceLength;
+    const earlyChange = lzwState.earlyChange;
+    let nextCode = lzwState.nextCode;
+    const dictionaryValues = lzwState.dictionaryValues;
+    const dictionaryLengths = lzwState.dictionaryLengths;
+    const dictionaryPrevCodes = lzwState.dictionaryPrevCodes;
+    let codeLength = lzwState.codeLength;
+    let prevCode = lzwState.prevCode;
+    const currentSequence = lzwState.currentSequence;
+    let currentSequenceLength = lzwState.currentSequenceLength;
 
-    var decodedLength = 0;
-    var currentBufferLength = this.bufferLength;
-    var buffer = this.ensureBuffer(this.bufferLength + estimatedDecodedSize);
+    let decodedLength = 0;
+    let currentBufferLength = this.bufferLength;
+    let buffer = this.ensureBuffer(this.bufferLength + estimatedDecodedSize);
 
     for (i = 0; i < blockSize; i++) {
-      var code = this.readBits(codeLength);
-      var hasPrev = currentSequenceLength > 0;
-      if (code < 256) {
+      const code = this.readBits(codeLength);
+      const hasPrev = currentSequenceLength > 0;
+      if (code === null || code < 256) {
         currentSequence[0] = code;
         currentSequenceLength = 1;
       } else if (code >= 258) {
@@ -113,9 +118,13 @@ var LZWStream = (function LZWStreamClosure() {
         dictionaryLengths[nextCode] = dictionaryLengths[prevCode] + 1;
         dictionaryValues[nextCode] = currentSequence[0];
         nextCode++;
-        codeLength = (nextCode + earlyChange) & (nextCode + earlyChange - 1) ?
-          codeLength : Math.min(Math.log(nextCode + earlyChange) /
-          0.6931471805599453 + 1, 12) | 0;
+        codeLength =
+          (nextCode + earlyChange) & (nextCode + earlyChange - 1)
+            ? codeLength
+            : Math.min(
+                Math.log(nextCode + earlyChange) / 0.6931471805599453 + 1,
+                12,
+              ) | 0;
       }
       prevCode = code;
 
@@ -136,9 +145,7 @@ var LZWStream = (function LZWStreamClosure() {
     lzwState.currentSequenceLength = currentSequenceLength;
 
     this.bufferLength = currentBufferLength;
-  };
+  }
+}
 
-  return LZWStream;
-})();
-
-module.exports = { LZWStream };
+export default LZWStream;
