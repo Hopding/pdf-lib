ef09c696452e17e0f7b63efad12bc2cbf2bff405
Andrew Dillon <andrew.dillon.j@gmail.com>
2019-07-02 21:03:26 -0500

    Add PDF links to README

--- scratchpad/index.ts
+++ scratchpad/index.ts
@@ -1,21 +1,48 @@
+import fontkit from '@pdf-lib/fontkit';
 import fs from 'fs';
-import { ParseSpeeds, PDFDocument } from 'src/api';
+import { PDFDocument, rgb } from 'src/index';
 
-const main = async () => {
-  // const inputBytes = fs.readFileSync('assets/pdfs/bixby_guide.pdf');
-  const inputBytes = fs.readFileSync(
-    'assets/pdfs/linearized_with_object_streams.pdf',
-  );
+(async () => {
+  // This should be a Uint8Array.
+  // This data can be obtained in a number of different ways.
+  // If your running in a Node environment, you could use fs.readFile().
+  // In the browser, you could make a fetch() call and use res.arrayBuffer().
+  const fontBytes = fs.readFileSync('./assets/fonts/ubuntu/Ubuntu-R.ttf');
 
-  console.time('parse');
-  const pdfDoc = await PDFDocument.load(inputBytes, {
-    parseSpeed: ParseSpeeds.Fastest,
+  const pdfDoc = await PDFDocument.create();
+
+  pdfDoc.registerFontkit(fontkit);
+
+  const customFont = await pdfDoc.embedFont(fontBytes);
+
+  const page = pdfDoc.addPage();
+
+  const text = 'This is text in an embedded font!';
+  const textSize = 35;
+  const textWidth = customFont.widthOfTextAtSize(text, textSize);
+  const textHeight = customFont.heightAtSize(textSize);
+
+  // Draw text on page
+  page.drawText(text, {
+    x: 40,
+    y: 450,
+    size: textSize,
+    font: customFont,
+    color: rgb(0, 0.53, 0.71),
   });
-  console.timeEnd('parse');
 
-  const buffer = await pdfDoc.save();
-  fs.writeFileSync('./out.pdf', buffer);
-  console.log('File written to ./out.pdf');
-};
+  // Draw a box around the text
+  page.drawRectangle({
+    x: 40,
+    y: 450,
+    width: textWidth,
+    height: textHeight,
+    borderColor: rgb(1, 0, 0),
+    borderWidth: 1.5,
+  });
 
-main();
+  const pdfBytes = await pdfDoc.save();
+
+  fs.writeFileSync('./out.pdf', pdfBytes);
+  console.log('./out.pdf');
+})();
