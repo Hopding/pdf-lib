4a46ddb990e635e180777a3436e9898bf19a446d
Andrew Dillon <andrew.dillon.j@gmail.com>
2019-07-21 16:45:17 -0500

    Finish documenting PDFFont

--- src/api/PDFFont.ts
+++ src/api/PDFFont.ts
@@ -9,12 +9,32 @@ import { assertIs } from 'src/utils';
 
 export type FontEmbedder = CustomFontEmbedder | StandardFontEmbedder;
 
+/**
+ * Represents a font that has been embedded in a [[PDFDocument]].
+ */
 export default class PDFFont {
+  /**
+   * > **NOTE:** You probably don't want to call this method directly. Instead,
+   * > consider using the [[PDFDocument.embedFont]] and
+   * > [[PDFDocument.embedStandardFont]] methods, which will create instances
+   * > of [[PDFFont]] for you.
+   *
+   * Create an instance of [[PDFFont]] from an existing ref and embedder
+   *
+   * @param ref The unique reference for this font.
+   * @param doc The document to which the font will belong.
+   * @param embedder The embedder that will be used to embed the font.
+   */
   static of = (ref: PDFRef, doc: PDFDocument, embedder: FontEmbedder) =>
     new PDFFont(ref, doc, embedder);
 
+  /** The unique reference assigned to this font within the document. */
   readonly ref: PDFRef;
+
+  /** The document to which this font belongs. */
   readonly doc: PDFDocument;
+
+  /** The name of this font. */
   readonly name: string;
 
   private modified = true;
@@ -35,28 +55,69 @@ export default class PDFFont {
     this.embedder = embedder;
   }
 
+  /**
+   * > **NOTE:** You probably don't need to call this method directly. The
+   * > [[PDFPage.drawText]] method will automatically encode the text it is
+   * > given.
+   *
+   * Encodes a string of text in this font.
+   *
+   * @param text The text to be encoded.
+   * @returns The encoded text as a hex string.
+   */
   encodeText(text: string): PDFHexString {
     assertIs(text, 'text', ['string']);
     this.modified = true;
     return this.embedder.encodeText(text);
   }
 
+  /**
+   * Measure the width of a string of text drawn in this font at a given size.
+   * For example:
+   * ```js
+   * const width = font.widthOfTextAtSize('Foo Bar Qux Baz', 36)
+   * ```
+   * @param text The string of text to be measured.
+   * @param size The font size to be used for this measurement.
+   * @returns The width of the string of text when drawn in this font at the
+   *          given size.
+   */
   widthOfTextAtSize(text: string, size: number): number {
     assertIs(text, 'text', ['string']);
     assertIs(size, 'size', ['number']);
     return this.embedder.widthOfTextAtSize(text, size);
   }
 
+  /**
+   * Measure the height of this font at a given size. For example:
+   * ```js
+   * const height = font.heightAtSize(24)
+   * ```
+   * @param size The font size to be used for this measurement.
+   * @returns The height of this font at the given size.
+   */
   heightAtSize(size: number): number {
     assertIs(size, 'size', ['number']);
     return this.embedder.heightOfFontAtSize(size);
   }
 
+  /**
+   * Compute the font size at which this font is a given height. For example:
+   * ```js
+   * const fontSize = font.sizeAtHeight(12)
+   * ```
+   * @param height The height to be used for this calculation.
+   * @returns The font size at which this font is the given height.
+   */
   sizeAtHeight(height: number): number {
     assertIs(height, 'height', ['number']);
     return this.embedder.sizeOfFontAtHeight(height);
   }
 
+  /**
+   * Get the set of unicode code points that can be represented by this font.
+   * @returns The set of unicode code points supported by this font.
+   */
   getCharacterSet(): number[] {
     if (this.embedder instanceof StandardFontEmbedder) {
       // TODO: Update @pdf-lib/standard fonts to export encoding.characterSet
@@ -68,8 +129,17 @@ export default class PDFFont {
     }
   }
 
-  // TODO: Cleanup orphan embedded objects if a font is embedded multiple times...
+  /**
+   * > **NOTE:** You probably don't need to call this method directly. The
+   * > [[PDFDocument.save]] and [[PDFDocument.saveAsBase64]] methods will
+   * > automatically ensure all fonts get embedded.
+   *
+   * Embed this font in its document.
+   *
+   * @returns Resolves when the embedding is complete.
+   */
   async embed(): Promise<void> {
+    // TODO: Cleanup orphan embedded objects if a font is embedded multiple times...
     if (this.modified) {
       await this.embedder.embedIntoContext(this.doc.context, this.ref);
       this.modified = false;
