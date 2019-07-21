4bf152855a7254b6f7f85ba15e969e859cf54143
Andrew Dillon <andrew.dillon.j@gmail.com>
2019-07-21 13:54:37 -0500

    Add node-fetch for development

--- scratchpad/index.ts
+++ scratchpad/index.ts
@@ -1,17 +1,47 @@
 import fs from 'fs';
-import { grayscale, PDFDocument } from 'src/index';
+import fetch from 'node-fetch';
 import { openPdf, Reader } from './open';
 
-(async () => {
+import fontkit from '@pdf-lib/fontkit';
+import { PDFDocument, rgb } from 'src/index';
+
+async function embedImages() {
+  const url = 'https://pdf-lib.js.org/assets/ubuntu/Ubuntu-R.ttf';
+  const fontBytes = await fetch(url).then((res) => res.arrayBuffer());
+
   const pdfDoc = await PDFDocument.create();
 
-  grayscale(21);
+  pdfDoc.registerFontkit(fontkit);
+  const customFont = await pdfDoc.embedFont(fontBytes);
 
-  pdfDoc.addPage();
+  const page = pdfDoc.addPage();
 
-  const pdfBytes = await pdfDoc.save({ useObjectStreams: 'foo' } as any);
+  const text = 'This is text in an embedded font!';
+  const textSize = 35;
+  const textWidth = customFont.widthOfTextAtSize(text, textSize);
+  const textHeight = customFont.heightAtSize(textSize);
+
+  page.drawText(text, {
+    x: 40,
+    y: 450,
+    size: textSize,
+    font: customFont,
+    color: rgb(0, 0.53, 0.71),
+  });
+  page.drawRectangle({
+    x: 40,
+    y: 450,
+    width: textWidth,
+    height: textHeight,
+    borderColor: rgb(1, 0, 0),
+    borderWidth: 1.5,
+  });
+
+  const pdfBytes = await pdfDoc.save();
 
   fs.writeFileSync('./out.pdf', pdfBytes);
 
   openPdf('./out.pdf', Reader.Preview);
-})();
+}
+
+embedImages();
