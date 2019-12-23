7d02ad309a3c80138adcbace63080ffd475daa99
Andrew Dillon <andrew.dillon.j@gmail.com>
2019-12-22 18:40:35 -0600

    Bump version to 1.3.0

--- tests/core/parser/PDFParser.spec.ts
+++ tests/core/parser/PDFParser.spec.ts
@@ -104,7 +104,11 @@ describe(`PDFParser`, () => {
     %PDF-1.7
     22 0 obj <</Type/Outlines/First ## 0 R/Last ** 0 R/Count 2>> endobj
   `;
-    const parser = PDFParser.forBytesWithOptions(typedArrayFor(input), 100, true);
+    const parser = PDFParser.forBytesWithOptions(
+      typedArrayFor(input),
+      100,
+      true,
+    );
     await expect(parser.parseDocument()).rejects.toBeInstanceOf(Error);
   });
 
