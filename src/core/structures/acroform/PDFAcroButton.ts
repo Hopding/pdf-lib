9e1012e35c0b470cb5c66ec58451c63e447f108b
Colin Weinshenker <caweinshenker2@gmail.com>
2019-12-30 15:00:42 -0700

    Add PushButton subclass of PDFAcroButton

--- src/core/structures/acroform/PDFAcroButton.ts
+++ src/core/structures/acroform/PDFAcroButton.ts
@@ -1,8 +1,13 @@
-import { PDFDict } from 'src/core';
-import { PDFTerminalField } from './index';
+import { PDFDict, PDFName, PDFNumber } from 'src/core';
+import { PDFTerminalField, PushButton } from './index';
 
 class PDFAcroButton extends PDFTerminalField {
   static fromDict(dict: PDFDict): PDFAcroButton {
+    const fieldFlags = dict.lookupMaybe(PDFName.Ff, PDFNumber)?.value() || 0;
+    const isPushButton = !!(fieldFlags & (1 << 17));
+    if (isPushButton) {
+      return new PushButton(dict);
+    }
     return new PDFAcroButton(dict);
   }
 }
