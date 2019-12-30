bbca2c7a107d5eb3a585f6410e7f530d8b34faea
Colin Weinshenker <caweinshenker2@gmail.com>
2019-12-29 18:16:02 -0700

    Add additional methods for AcroChoice-specific field properties

--- tests/core/structures/acroform/PDFAcroChoice.spec.ts
+++ tests/core/structures/acroform/PDFAcroChoice.spec.ts
@@ -1,9 +1,12 @@
 import {
   DictMap,
   PDFAcroChoice,
+  PDFArray,
   PDFContext,
   PDFDict,
   PDFName,
+  PDFNumber,
+  PDFObject,
 } from 'src/index';
 
 describe('PDFAcroChoice', () => {
@@ -19,5 +22,115 @@ describe('PDFAcroChoice', () => {
     const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
     const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
     expect(choiceField).toBeInstanceOf(PDFAcroChoice);
+  });
+
+  it('can return the options array', () => {
+    const options = PDFArray.withContext(context);
+    dict = new Map<PDFName, PDFObject>([
+      [PDFName.Opt, options],
+      [PDFName.FT, PDFName.Ch],
+    ]);
+    const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
+    const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
+    expect(choiceField.Opt()).toBe(options);
+  });
+
+  it('can return the top index', () => {
+    const topIndex = PDFNumber.of(1);
+    dict = new Map<PDFName, PDFObject>([
+      [PDFName.FT, PDFName.Ch],
+      [PDFName.TI, topIndex]
+    ]);
+    const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
+    const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
+    expect(choiceField.TI()).toEqual(PDFNumber.of(1));
+  });
+
+  it('can return the selection options indices array', () => {
+    const optionIndicesArray = PDFArray.withContext(context);
+    dict = new Map<PDFName, PDFObject>([
+      [PDFName.I, optionIndicesArray],
+      [PDFName.FT, PDFName.Ch],
+    ]);
+    const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
+    const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
+    expect(choiceField.I()).toBe(optionIndicesArray);
+  });
+
+  it('can return whether the choice field is a combo box', () => {
+    const fieldFlags = PDFNumber.of(1 << 18);
+    dict = new Map<PDFName, PDFObject>([
+      [PDFName.FT, PDFName.Ch],
+      [PDFName.Ff, fieldFlags]
+    ]);
+    const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
+    const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
+    expect(choiceField.isCombo()).toBe(true);
+  });
+
+  it('can return whether the choice field has an editable text box', () => {
+    const fieldFlags = PDFNumber.of(1 << 19);
+    dict = new Map<PDFName, PDFObject>([
+      [PDFName.FT, PDFName.Ch],
+      [PDFName.Ff, fieldFlags]
+    ]);
+    const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
+    const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
+    expect(choiceField.isEditable()).toBe(true);
+  });
+
+  it('can return whether the choices are sorted alphabetically', () => {
+    const fieldFlags = PDFNumber.of(1 << 20);
+    dict = new Map<PDFName, PDFObject>([
+      [PDFName.FT, PDFName.Ch],
+      [PDFName.Ff, fieldFlags]
+    ]);
+    const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
+    const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
+    expect(choiceField.isSorted()).toBe(true);
+  });
+
+  it('can return whether the field is a multi-select', () => {
+    const fieldFlags = PDFNumber.of(1 << 22);
+    dict = new Map<PDFName, PDFObject>([
+      [PDFName.FT, PDFName.Ch],
+      [PDFName.Ff, fieldFlags]
+    ]);
+    const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
+    const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
+    expect(choiceField.isMultiSelect()).toBe(true);
+  });
+
+  it('can return whether conformed readers will spell-check the field', () => {
+    const fieldFlags = PDFNumber.of(1 << 23);
+    dict = new Map<PDFName, PDFObject>([
+      [PDFName.FT, PDFName.Ch],
+      [PDFName.Ff, fieldFlags]
+    ]);
+    const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
+    const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
+    expect(choiceField.isSpellChecked()).toBe(false);
+  });
+
+  it('can return whether conformed readers will spell-check the field', () => {
+    const fieldFlags = PDFNumber.of(1 << 23);
+    dict = new Map<PDFName, PDFObject>([
+      [PDFName.FT, PDFName.Ch],
+      [PDFName.Ff, fieldFlags]
+    ]);
+    const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
+    const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
+    expect(choiceField.isSpellChecked()).toBe(false);
+  });
+
+  it('can return whether a selection change is committed before the user leaves the field', () => {
+    const fieldFlags = PDFNumber.of(1 << 27);
+    dict = new Map<PDFName, PDFObject>([
+      [PDFName.FT, PDFName.Ch],
+      [PDFName.Ff, fieldFlags]
+    ]);
+    const acroFormFieldDict = PDFDict.fromMapWithContext(dict, context);
+    const choiceField = PDFAcroChoice.fromDict(acroFormFieldDict);
+    expect(choiceField.isCommittedOnSelChange()).toBe(true);
   });
 });
