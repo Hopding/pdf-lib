bcf1a3e8f2a4cc5cc002bc5a22973f75656c1031
cshenks <58759099+cshenks@users.noreply.github.com>
2020-01-20 10:36:48 -0700

    Annotation dict (#312)
    * Add PDFAnnotation base
    
    * Add remaining tests for PDFAnnotation raw attribute methods
    
    * Add PDFRectangle structure
    
    * Export PDFRectangle
    
    * Add PDFAnnotation.Rect
    
    * Lint
    
    * Return PDFArray from PDFAnnotation.Rect
    
    * Remove normalization method for PDFRectangle.
    
    A way to change the rect dimensions will be handled in
    https://github.com/Hopding/pdf-lib/issues/320
    
    Co-authored-by: Colin Weinshenker <caweinshenker@users.noreply.github.com>

---
+++ tests/core/structures/PDFRectangle.spec.ts
@@ -0,0 +1,32 @@
+import { PDFArray, PDFContext, PDFNumber, PDFRectangle } from 'src/core';
+
+describe(`PDFRectangle`, () => {
+  const context = PDFContext.create();
+  let array: PDFArray;
+  let rectangle: PDFRectangle;
+
+  beforeEach(() => {
+    array = PDFArray.withContext(context);
+    array.push(PDFNumber.of(1));
+    array.push(PDFNumber.of(2));
+    array.push(PDFNumber.of(3));
+    array.push(PDFNumber.of(4));
+    rectangle = PDFRectangle.fromArray(array);
+  });
+
+  it('can return the lower left X coordinate', () => {
+    expect(rectangle.lowerLeftX()).toEqual(1);
+  });
+
+  it('can return the lower left Y coordinate', () => {
+    expect(rectangle.lowerLeftY()).toEqual(2);
+  });
+
+  it('can return the upper right X coordinate', () => {
+    expect(rectangle.upperRightX()).toEqual(3);
+  });
+
+  it('can return the upper right Y coordinate', () => {
+    expect(rectangle.upperRightY()).toEqual(4);
+  });
+});
