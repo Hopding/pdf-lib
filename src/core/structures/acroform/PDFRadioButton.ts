d3a93eb83d09dd48044a2d9d031db72e2dedf450
Colin Weinshenker <caweinshenker2@gmail.com>
2020-01-19 18:23:45 -0700

    Add PDFRadioButton toggle methods for selecting/deselecting children

--- src/core/structures/acroform/PDFRadioButton.ts
+++ src/core/structures/acroform/PDFRadioButton.ts
@@ -1,8 +1,13 @@
-import { PDFArray, PDFDict, PDFName } from 'src/core';
+import {
+  PDFAnnotation,
+  PDFArray,
+  PDFDict,
+  PDFName,
+} from 'src/core';
 import { PDFAcroButton } from './index';
 
 class PDFRadioButton extends PDFAcroButton {
-  static fromDict(dict: PDFDict): PDFAcroButton {
+  static fromDict(dict: PDFDict): PDFRadioButton {
     return new PDFRadioButton(dict);
   }
 
@@ -25,7 +30,7 @@ class PDFRadioButton extends PDFAcroButton {
     return !!(this.getFlags() | (1 << 15));
   }
 
-  RadiosInUnsion(): boolean {
+  RadiosInUnison(): boolean {
     return !!(this.getFlags() | (1 << 26));
   }
 
@@ -37,16 +42,61 @@ class PDFRadioButton extends PDFAcroButton {
       return this.Kids().lookupElements(PDFDict);
   }
 
-  select(index: number): void {
-      // TODO: Refactor to use annotation dictionary structure. See #309
-      const kids = this.getKids();
-      const selectedChild = kids[index];
-      const childAppearanceDictionary = selectedChild.lookup(PDFName.AP, PDFDict);
-      const childNormalAppearanceDict = childAppearanceDictionary.lookup(PDFName.N);
-      const childNormalAppearanceOptions = childAppearanceDictionary.entries();
-      const childNormalAppearanceOnValue = childNormalAppearanceOptions.filter(option => option[0] !== PDFName.Off)[0][0];
+  getValue(): PDFName {
+    const value = this.dict.lookup(PDFName.V);
+    if (!value) {
+      return PDFName.Off;
+    }
+    return this.dict.lookup(PDFName.V, PDFName);
+  }
 
-      this.setValue();
+  _getChildOnAppearanceState(index: number): PDFName {
+    const kids = this.getKids();
+    const childAnnotation = PDFAnnotation.fromDict(kids[index]);
+    const childAnnotationNormalAppearanceDict = childAnnotation.dict.lookup(PDFName.AP, PDFDict).lookup(PDFName.N, PDFDict);
+    const childAnnotationNormalOnAppearance = childAnnotationNormalAppearanceDict.entries().filter(entry => entry[0] !== PDFName.Off)[0][0];
+    return childAnnotationNormalOnAppearance;
+  }
+
+  _toggleChild(index: number): void {
+   console.log(this.getValue());
+   if (this.getValue() === PDFName.Off) {
+    this.setValue(this._getChildOnAppearanceState(index));
+   } else {
+    this.setValue(PDFName.Off);
+   }
+  }
+
+  _toggleInUnison(index: number): void {
+    const childOnValues = this.getKids().map((_child, childIndex) => this._getChildOnAppearanceState(childIndex));
+    const targetOnValue = childOnValues[index];
+    childOnValues.forEach((onValue, childIndex) => {
+      if (onValue === targetOnValue) {
+        this._toggleChild(childIndex);
+      }
+    });
+  }
+
+  _toggleWithNoToggleToOff(index: number) {
+    if (this.getValue() === PDFName.Off) {
+      this._toggleChild(index);
+    }
+  }
+
+  toggle(index: number): void {
+    console.log(this.getFlags());
+    if (this.RadiosInUnison() && this.NoToggleToOff()) {
+      console.log("HERE");
+      if (this.getValue() === PDFName.Off) {
+        this._toggleInUnison(index);
+      }
+    } else if (this.RadiosInUnison()) {
+      this._toggleInUnison(index);
+    } else if (this.NoToggleToOff()) {
+      this._toggleWithNoToggleToOff(index);
+    } else {
+      this._toggleChild(index);
+    }
   }
 }
 
