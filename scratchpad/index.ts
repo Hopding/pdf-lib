af4adf5a2acb0dea7b6b8ab1760f938b31974435
Philipp Tessenow <philipp@tessenow.org>
2020-02-09 14:12:28 +0100

    use transformation options in scratchpad

--- scratchpad/index.ts
+++ scratchpad/index.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import { openPdf, Reader } from './open';
 
+import { degrees } from 'src/api/rotations';
 import { PDFDocument } from 'src/index';
 
 (async () => {
@@ -8,7 +9,33 @@ import { PDFDocument } from 'src/index';
 
   const page = pdfDoc.addPage();
 
-  page.drawText('Hello, world!');
+  // taken from: https://pdf-lib.js.org/docs/api/classes/pdfdocument#static-load
+  const base64 =
+    'JVBERi0xLjcKJYGBgYEKCjUgMCBvYmoKPDwKL0ZpbHRlciAvRmxhdGVEZWNvZGUKL0xlbm' +
+    'd0aCAxMDQKPj4Kc3RyZWFtCniccwrhMlAAwaJ0Ln2P1Jyy1JLM5ERdc0MjCwUjE4WQNC4Q' +
+    '6cNlCFZkqGCqYGSqEJLLZWNuYGZiZmbkYuZsZmlmZGRgZmluDCQNzc3NTM2NzdzMXMxMjQ' +
+    'ztFEKyuEK0uFxDuAAOERdVCmVuZHN0cmVhbQplbmRvYmoKCjYgMCBvYmoKPDwKL0ZpbHRl' +
+    'ciAvRmxhdGVEZWNvZGUKL1R5cGUgL09ialN0bQovTiA0Ci9GaXJzdCAyMAovTGVuZ3RoID' +
+    'IxNQo+PgpzdHJlYW0KeJxVj9GqwjAMhu/zFHkBzTo3nCCCiiKIHPEICuJF3cKoSCu2E8/b' +
+    '20wPIr1p8v9/8kVhgilmGfawX2CGaVrgcAi0/bsy0lrX7IGWpvJ4iJYEN3gEmrrGBlQwGs' +
+    'HHO9VBX1wNrxAqMX87RBD5xpJuddqwd82tjAHxzV1U5LPgy52DKXWnr1Lheg+j/c/pzGVr' +
+    'iqV0VlwZPXGPCJjElw/ybkwUmeoWgxesDXGhHJC/D/iikp1Av80ptKU0FdBEe25pPihAM1' +
+    'u6ytgaaWfs2Hrz35CJT1+EWmAKZW5kc3RyZWFtCmVuZG9iagoKNyAwIG9iago8PAovU2l6' +
+    'ZSA4Ci9Sb290IDIgMCBSCi9GaWx0ZXIgL0ZsYXRlRGVjb2RlCi9UeXBlIC9YUmVmCi9MZW' +
+    '5ndGggMzgKL1cgWyAxIDIgMiBdCi9JbmRleCBbIDAgOCBdCj4+CnN0cmVhbQp4nBXEwREA' +
+    'EBAEsCwz3vrvRmOOyyOoGhZdutHN2MT55fIAVocD+AplbmRzdHJlYW0KZW5kb2JqCgpzdG' +
+    'FydHhyZWYKNTEwCiUlRU9G';
+  const pdfDoc1 = await PDFDocument.load(base64);
+  const embeddedPage = await pdfDoc.embedPdfDocument(pdfDoc1);
+  page1.drawEmbeddedPdfPage(embeddedPage, {
+    x: 200,
+    y: 200,
+    xScale: 0.2,
+    yScale: 0.2,
+    // xSkew: degrees(20),
+    // ySkew: degrees(80),
+    rotate: degrees(45),
+  });
 
   const pdfBytes = await pdfDoc.save();
 
