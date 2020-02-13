176b3513fa72caf36721b721acf4c8f8189bcec5
Philipp Tessenow <philipp@tessenow.org>
2020-02-13 11:50:38 +0100

    added EmbeddedPDFPages to the list of ignore compilation warnings regarding circular dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -5,15 +5,16 @@ import { terser } from 'rollup-plugin-terser';
 
 const { MINIFY } = process.env;
 
-const WarningBlacklist = [
+const IgnoredWarnings = [
   'Circular dependency: es/api/PDFDocument.js -> es/api/PDFFont.js -> es/api/PDFDocument.js',
   'Circular dependency: es/api/PDFDocument.js -> es/api/PDFImage.js -> es/api/PDFDocument.js',
   'Circular dependency: es/api/PDFDocument.js -> es/api/PDFPage.js -> es/api/PDFDocument.js',
+  'Circular dependency: es/api/PDFDocument.js -> es/api/EmbeddedPDFPage.js -> es/api/PDFDocument.js',
 ];
 
 // Silence circular dependency warnings we don't care about
 const onwarn = (warning, warn) => {
-  if (WarningBlacklist.includes(warning.message)) return;
+  if (IgnoredWarnings.includes(warning.message)) return;
   warn(warning);
 };
 
