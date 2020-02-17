3da108961fe3d28036ce32969daf8c40a37a0d59
Andrew Dillon <andrew.dillon.j@gmail.com>
2020-02-17 11:11:11 -0600

    Replace png-ts with upng-js

--- scratchpad/index.ts
+++ scratchpad/index.ts
@@ -1,43 +1,26 @@
 import fs from 'fs';
 import { openPdf, Reader } from './open';
 
-import { PDFDocument, rgb } from 'src/index';
+import { PDFDocument } from 'src/index';
 
 (async () => {
-  // SVG path for a wavy line
-  const svgPath =
-    'M 0,20 L 100,160 Q 130,200 150,120 C 190,-40 200,200 300,150 L 400,90';
-
-  // Create a new PDFDocument
   const pdfDoc = await PDFDocument.create();
 
-  // Add a blank page to the document
+  const smallMario = await pdfDoc.embedPng(
+    // fs.readFileSync('assets/images/small_mario.png'),
+    fs.readFileSync('assets/images/greyscale_bird.png'),
+  );
+  const dims = smallMario.scale(1);
+
   const page = pdfDoc.addPage();
-  page.moveTo(100, page.getHeight() - 5);
 
-  // Draw the SVG path as a black line
-  page.moveDown(25);
-  page.drawSvgPath(svgPath);
+  page.drawImage(smallMario, {
+    ...dims,
+    x: page.getWidth() / 2 - dims.width / 2,
+    y: page.getHeight() / 2 - dims.height / 2,
+  });
 
-  // Draw the SVG path as a thick green line
-  page.moveDown(200);
-  page.drawSvgPath(svgPath, { borderColor: rgb(0, 1, 0), borderWidth: 5 });
-
-  // Draw the SVG path and fill it with red
-  page.moveDown(200);
-  page.drawSvgPath(svgPath, { color: rgb(1, 0, 0) });
-
-  // Draw the SVG path at 50% of its original size
-  page.moveDown(200);
-  page.drawSvgPath(svgPath, { scale: 0.5 });
-
-  // Serialize the PDFDocument to bytes (a Uint8Array)
   const pdfBytes = await pdfDoc.save();
-
-  // For example, `pdfBytes` can be:
-  //   • Written to a file in Node
-  //   • Downloaded from the browser
-  //   • Rendered in an <iframe>
 
   fs.writeFileSync('./out.pdf', pdfBytes);
 
--- src/core/embedders/PngEmbedder.ts
+++ src/core/embedders/PngEmbedder.ts
@@ -1,15 +1,6 @@
-import pako from 'pako';
-import PNG, { ColorSpace as ColorSpaceType } from 'png-ts';
-import UPNG from 'upng-js';
-
 import PDFRef from 'src/core/objects/PDFRef';
 import PDFContext from 'src/core/PDFContext';
-
-const getColorSpace = (colorType: number) => {
-  if (colorType === 1) return 'DeviceGray';
-  if (colorType === 3) return 'DeviceRGB';
-  else throw new Error(`Unknown PNG color type: ${colorType}`);
-};
+import { PNG } from 'src/utils/png';
 
 /**
  * A note of thanks to the developers of https://github.com/foliojs/pdfkit, as
@@ -18,95 +9,36 @@ const getColorSpace = (colorType: number) => {
  */
 class PngEmbedder {
   static async for(imageData: Uint8Array) {
-    const image = await PNG.load(imageData);
-    const uImage = UPNG.decode(imageData);
-    return new PngEmbedder(image, uImage);
+    const png = PNG.load(imageData);
+    return new PngEmbedder(png);
   }
 
   readonly bitsPerComponent: number;
   readonly height: number;
   readonly width: number;
-  readonly colorSpace: ColorSpaceType;
+  readonly colorSpace: 'DeviceRGB';
 
-  // private readonly image: PNG;
-  private readonly uImage: UPNG.Image;
-  private imageData: Uint8Array;
-  private alphaChannel: Uint8Array | undefined;
+  private readonly image: PNG;
 
-  private constructor(png: PNG, upng: UPNG.Image) {
+  private constructor(png: PNG) {
     this.image = png;
-    this.uImage = upng;
-    this.imageData = this.image.imgData;
-    this.alphaChannel = undefined;
-
-    // this.bitsPerComponent = this.image.bits;
-    // this.height = this.image.height;
-    // this.width = this.image.width;
-    // this.colorSpace = this.image.colorSpace;
-
-    this.bitsPerComponent = this.uImage.depth;
-    this.height = this.uImage.height;
-    this.width = this.uImage.width;
-    this.colorSpace = getColorSpace(this.uImage.ctype);
-    console.log('C_TYPE:', this.uImage.ctype);
-
-    // TODO: Handle the following two transparency types. They don't seem to be
-    // fully handled in:
-    // https://github.com/devongovett/pdfkit/blob/e71edab0dd4657b5a767804ba86c94c58d01fbca/lib/image/png.coffee
-    //
-    // if (this.image.transparency.grayscale)
-    // if (this.image.transparency.rgb)
+    this.bitsPerComponent = png.bitsPerComponent;
+    this.width = png.width;
+    this.height = png.height;
+    this.colorSpace = 'DeviceRGB';
   }
 
   async embedIntoContext(context: PDFContext, ref?: PDFRef): Promise<PDFRef> {
-    if (!this.alphaChannel) {
-      if (this.image.transparency.indexed) {
-        await this.loadIndexedAlphaChannel();
-      } else if (this.image.hasAlphaChannel) {
-        await this.splitAlphaChannel();
-      }
-    }
-
     const SMask = this.embedAlphaChannel(context);
 
-    const { palette } = this.image;
-
-    // let ColorSpace: string | any[] = this.image.colorSpace;
-    let ColorSpace: string | any[] = this.colorSpace;
-    if (palette.length !== 0) {
-      const stream = context.stream(new Uint8Array(palette));
-      const streamRef = context.register(stream);
-      ColorSpace = ['Indexed', 'DeviceRGB', palette.length / 3 - 1, streamRef];
-    }
-
-    let DecodeParms;
-    if (!this.image.hasAlphaChannel) {
-      DecodeParms = {
-        Predictor: 15,
-        Colors: this.image.colors,
-
-        // BitsPerComponent: this.image.bits,
-        // Columns: this.image.width,
-        BitsPerComponent: this.uImage.depth,
-        Columns: this.uImage.width,
-      };
-    }
-
-    const xObject = context.stream(this.imageData, {
+    const xObject = context.flateStream(this.image.rgbChannel, {
       Type: 'XObject',
       Subtype: 'Image',
-
-      // BitsPerComponent: this.image.bits,
-      // Width: this.image.width,
-      // Height: this.image.height,
-      BitsPerComponent: this.uImage.depth,
-      Width: this.uImage.width,
-      Height: this.uImage.height,
-
-      Filter: 'FlateDecode',
+      BitsPerComponent: this.image.bitsPerComponent,
+      Width: this.image.width,
+      Height: this.image.height,
+      ColorSpace: this.colorSpace,
       SMask,
-      DecodeParms,
-      ColorSpace,
     });
 
     if (ref) {
@@ -118,67 +50,17 @@ class PngEmbedder {
   }
 
   private embedAlphaChannel(context: PDFContext): PDFRef | undefined {
-    if (!this.alphaChannel) return undefined;
-    const xObject = context.flateStream(this.alphaChannel, {
+    if (!this.image.alphaChannel) return undefined;
+    const xObject = context.flateStream(this.image.alphaChannel, {
       Type: 'XObject',
       Subtype: 'Image',
-
-      // Height: this.image.height,
-      // Width: this.image.width,
-      Height: this.uImage.height,
-      Width: this.uImage.width,
-
-      BitsPerComponent: 8,
+      Height: this.image.height,
+      Width: this.image.width,
+      BitsPerComponent: this.image.bitsPerComponent,
       ColorSpace: 'DeviceGray',
       Decode: [0, 1],
     });
     return context.register(xObject);
-  }
-
-  private async splitAlphaChannel(): Promise<void> {
-    // const { colors, bits, width, height } = this.image;
-    const { ctype, depth, width, height, data } = this.uImage;
-
-    // const pixels = this.image.decodePixels();
-    const pixels = new Uint8Array(data);
-
-    // const colorByteSize = (colors * bits) / 8;
-    const colorByteSize = (ctype * depth) / 8;
-
-    const pixelCount = width * height;
-
-    const imageData = new Uint8Array(pixelCount * colorByteSize);
-    const alphaChannel = new Uint8Array(pixelCount);
-
-    let pixelOffset = 0;
-    let rgbOffset = 0;
-    let alphaOffset = 0;
-
-    const { length } = pixels;
-    while (pixelOffset < length) {
-      imageData[rgbOffset++] = pixels[pixelOffset++];
-      imageData[rgbOffset++] = pixels[pixelOffset++];
-      imageData[rgbOffset++] = pixels[pixelOffset++];
-      alphaChannel[alphaOffset++] = pixels[pixelOffset++];
-    }
-
-    this.imageData = pako.deflate(imageData);
-    this.alphaChannel = alphaChannel;
-  }
-
-  private async loadIndexedAlphaChannel(): Promise<void> {
-    const transparency = this.image.transparency.indexed!;
-
-    // const pixels = this.image.decodePixels();
-    const pixels = new Uint8Array(this.uImage.data);
-
-    const alphaChannel = new Uint8Array(this.image.width * this.image.height);
-
-    for (let idx = 0, len = pixels.length; idx < len; idx++) {
-      alphaChannel[idx] = transparency[pixels[idx]];
-    }
-
-    this.alphaChannel = alphaChannel;
   }
 }
 
