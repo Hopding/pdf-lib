79bd4e3d7958d432bfc56953fb7a9a4b5d803539
Andrew Dillon <andrew.dillon.j@gmail.com>
2020-02-20 19:20:15 -0600

    Update tests

--- scratchpad/index.ts
+++ scratchpad/index.ts
@@ -6,24 +6,9 @@ import { PDFDocument } from 'src/index';
 (async () => {
   const pdfDoc = await PDFDocument.create();
 
-  const smallMario = await pdfDoc.embedPng(
-    // fs.readFileSync('assets/images/small_mario.png'),
-    // fs.readFileSync('assets/images/greyscale_bird.png'),
-    // fs.readFileSync('/Users/user/Desktop/signature_export.png'),
-    // fs.readFileSync('/Users/user/Desktop/signature.png'),
-    // fs.readFileSync('/Users/user/Desktop/upng-test/suite/basi0g01.png'),
-    // fs.readFileSync('/Users/user/Desktop/bw_export.png'),
-    fs.readFileSync('/Users/user/Desktop/etwe.png'),
-  );
-  const dims = smallMario.scale(1);
-
   const page = pdfDoc.addPage();
 
-  page.drawImage(smallMario, {
-    ...dims,
-    x: page.getWidth() / 2 - dims.width / 2,
-    y: page.getHeight() / 2 - dims.height / 2,
-  });
+  page.drawText('Hello, world!');
 
   const pdfBytes = await pdfDoc.save();
 
