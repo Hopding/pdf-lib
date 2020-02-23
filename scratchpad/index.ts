631ddb024996eb99f6307e28b020d9fd77a19a5f
Philipp Tessenow <philipp@tessenow.org>
2020-02-23 12:35:04 +0100

    reset scratchpad to master again

--- scratchpad/index.ts
+++ scratchpad/index.ts
@@ -1,35 +1,14 @@
 import fs from 'fs';
 import { openPdf, Reader } from './open';
 
-import { PDFDocument, rgb } from 'src/index';
+import { PDFDocument } from 'src/index';
 
 (async () => {
-  // SVG path for a wavy line
-  const svgPath =
-    'M 0,20 L 100,160 Q 130,200 150,120 C 190,-40 200,200 300,150 L 400,90';
-
-  // Create a new PDFDocument
   const pdfDoc = await PDFDocument.create();
 
-  // Add a blank page to the document
   const page = pdfDoc.addPage();
-  page.moveTo(100, page.getHeight() - 5);
 
-  // Draw the SVG path as a black line
-  page.moveDown(25);
-  page.drawSvgPath(svgPath);
-
-  // Draw the SVG path as a thick green line
-  page.moveDown(200);
-  page.drawSvgPath(svgPath, { borderColor: rgb(0, 1, 0), borderWidth: 5 });
-
-  // Draw the SVG path and fill it with red
-  page.moveDown(200);
-  page.drawSvgPath(svgPath, { color: rgb(1, 0, 0) });
-
-  // Draw the SVG path at 50% of its original size
-  page.moveDown(200);
-  page.drawSvgPath(svgPath, { scale: 0.5 });
+  page.drawText('Hello, world!');
 
   const pdfBytes = await pdfDoc.save();
 
