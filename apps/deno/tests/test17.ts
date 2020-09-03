bb8f5ffd75490c68625569a8e5bf93a6c885c6ca
Andrew Dillon <andrew.dillon.j@gmail.com>
2020-09-03 17:23:47 -0500

    Update test17 in all envs

--- apps/deno/tests/test17.ts
+++ apps/deno/tests/test17.ts
@@ -1,7 +1,17 @@
 import fontkit from 'https://cdn.pika.dev/@pdf-lib/fontkit@^1.0.0';
 
 import { Assets } from '../index.ts';
-import { PDFDocument } from '../../../dist/pdf-lib.esm.js';
+import {
+  PDFDocument,
+  StandardFonts,
+  drawRectangle,
+  rgb,
+  degrees,
+  drawText,
+  PDFFont,
+  drawEllipse,
+  PDFWidgetAnnotation,
+} from '../../../dist/pdf-lib.esm.js';
 
 export default async (assets: Assets) => {
   const pdfDoc = await PDFDocument.load(assets.pdfs.fancy_fields);
@@ -63,6 +73,112 @@ export default async (assets: Assets) => {
   // Dropdown
   const gundams = form.getDropdown('Choose A Gundam 🤖');
   gundams.updateAppearances(ubuntuFont);
+
+  // ===================== Custom Appearance Providers ========================
+  const page = pdfDoc.addPage();
+  const symbol = await pdfDoc.embedFont(StandardFonts.Symbol);
+
+  const btn = form.createButton('custom.button.field');
+  const cb = form.createCheckBox('custom.checkbox.field');
+  const dd = form.createDropdown('custom.dropdown.field');
+  const ol = form.createOptionList('custom.optionlist.field');
+  const rg = form.createRadioGroup('custom.radiogroup.field');
+  const tf = form.createTextField('custom.text.field');
+
+  dd.addOptions('∑');
+  ol.addOptions('∑');
+
+  const width = 100;
+  const height = 50;
+  const x = page.getWidth() / 2 - width / 2;
+  let y = page.getHeight();
+
+  y -= height + 25;
+  btn.addToPage('∑', symbol, page, { x, y, width, height });
+  y -= height + 25;
+  cb.addToPage(page, { x, y, width, height });
+  y -= height + 25;
+  dd.addToPage(symbol, page, { x, y, width, height });
+  y -= height + 25;
+  ol.addToPage(symbol, page, { x, y, width, height });
+  y -= height + 25;
+  rg.addOptionToPage('bar', page, { x, y, width, height });
+  y -= height + 25;
+  tf.addToPage(symbol, page, { x, y, width, height });
+
+  const rectangle = drawRectangle({
+    x: 0,
+    y: 0,
+    width,
+    height,
+    borderWidth: 2,
+    color: rgb(1, 0.1, 0.75),
+    borderColor: rgb(0, 0.1, 1),
+    rotate: degrees(0),
+    xSkew: degrees(0),
+    ySkew: degrees(0),
+  });
+
+  const circle = drawEllipse({
+    x: 15,
+    y: height - 15,
+    xScale: 15,
+    yScale: 15,
+    borderWidth: 0,
+    color: rgb(0, 0, 0),
+    borderColor: undefined,
+    rotate: degrees(0),
+  });
+
+  const text = symbol.encodeText('ℑ');
+  const textW = symbol.widthOfTextAtSize('ℑ', 35);
+  const textH = symbol.heightAtSize(35);
+  const symbolText = (font: PDFFont) =>
+    drawText(text, {
+      x: width / 2 - textW / 2,
+      y: height / 2 - textH / 2 + 10,
+      color: rgb(0, 0, 0),
+      font: font.name,
+      size: 35,
+      rotate: degrees(0),
+      xSkew: degrees(0),
+      ySkew: degrees(0),
+    });
+
+  const assert = (condition: boolean, msg = '') => {
+    if (!condition) throw new Error(msg || 'Assertion failed');
+  };
+
+  btn.updateAppearances(symbol, (field: any, widget: any, font: any) => {
+    assert(field === btn);
+    assert(widget instanceof PDFWidgetAnnotation);
+    return [...rectangle, ...symbolText(font)];
+  });
+  cb.updateAppearances((field: any, widget: any) => {
+    assert(field === cb);
+    assert(widget instanceof PDFWidgetAnnotation);
+    return { on: [...rectangle, ...circle], off: [...rectangle, ...circle] };
+  });
+  dd.updateAppearances(symbol, (field: any, widget: any, font: any) => {
+    assert(field === dd);
+    assert(widget instanceof PDFWidgetAnnotation);
+    return [...rectangle, ...symbolText(font)];
+  });
+  ol.updateAppearances(symbol, (field: any, widget: any, font: any) => {
+    assert(field === ol);
+    assert(widget instanceof PDFWidgetAnnotation);
+    return [...rectangle, ...symbolText(font)];
+  });
+  rg.updateAppearances((field: any, widget: any) => {
+    assert(field === rg);
+    assert(widget instanceof PDFWidgetAnnotation);
+    return { on: [...rectangle, ...circle], off: [...rectangle, ...circle] };
+  });
+  tf.updateAppearances(symbol, (field: any, widget: any, font: any) => {
+    assert(field === tf);
+    assert(widget instanceof PDFWidgetAnnotation);
+    return [...rectangle, ...symbolText(font)];
+  });
 
   const pdfBytes = await pdfDoc.save();
   return pdfBytes;
