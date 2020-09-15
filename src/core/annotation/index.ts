1ed8ea6002fe5d4920bd28552e1fa38127defe7f
Andrew Dillon <andrew.dillon.j@gmail.com>
2020-09-15 18:24:39 -0500

    Make new widgets printable; Fix AcroField type error

--- src/core/annotation/index.ts
+++ src/core/annotation/index.ts
@@ -1,3 +1,4 @@
 export { default as PDFAnnotation } from 'src/core/annotation/PDFAnnotation';
 export { default as PDFWidgetAnnotation } from 'src/core/annotation/PDFWidgetAnnotation';
 export { default as AppearanceCharacteristics } from 'src/core/annotation/AppearanceCharacteristics';
+export * from 'src/core/annotation/flags';
