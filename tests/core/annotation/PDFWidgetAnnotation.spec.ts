d197ac66e0c176de4b34387abc47a3504764bf9c
Andrew Dillon <andrew.dillon.j@gmail.com>
2020-12-06 13:22:09 -0600

    Support Hex String Default Appearances (#693)
    * Support hexadecimal Default Appearance (#677)
    
    * Add test for PDFAcroField.getDefaultAppearance
    
    * Add test for PDFWidgetAnnotation.getDefaultAppearance
    
    * Cleanup
    
    Co-authored-by: Bj Tecu <btecu@users.noreply.github.com>

---
+++ tests/core/annotation/PDFWidgetAnnotation.spec.ts
@@ -0,0 +1,40 @@
+import {
+  PDFWidgetAnnotation,
+  PDFContext,
+  PDFString,
+  PDFHexString,
+  PDFName,
+  PDFNull,
+} from 'src/index';
+
+describe(`PDFWidgetAnnotation`, () => {
+  it(`returns undefined for missing (DAs)`, () => {
+    const context = PDFContext.create();
+
+    const parentRef = context.nextRef();
+    const widget = PDFWidgetAnnotation.create(context, parentRef);
+    widget.dict.set(PDFName.of('DA'), PDFNull);
+
+    expect(widget.getDefaultAppearance()).toBe(undefined);
+  });
+
+  it(`returns normal direct appearance strings (DAs)`, () => {
+    const context = PDFContext.create();
+
+    const parentRef = context.nextRef();
+    const widget = PDFWidgetAnnotation.create(context, parentRef);
+    widget.dict.set(PDFName.of('DA'), PDFString.of('/ZaDb 10 Tf 0 g'));
+
+    expect(widget.getDefaultAppearance()).toBe('/ZaDb 10 Tf 0 g');
+  });
+
+  it(`returns hexadecimal (non-standard) direct appearance strings (DAs)`, () => {
+    const context = PDFContext.create();
+
+    const parentRef = context.nextRef();
+    const widget = PDFWidgetAnnotation.create(context, parentRef);
+    widget.dict.set(PDFName.of('DA'), PDFHexString.fromText('/ZaDb 10 Tf 0 g'));
+
+    expect(widget.getDefaultAppearance()).toBe('/ZaDb 10 Tf 0 g');
+  });
+});
