364670a5280f153c30248cc263f21fd53552c93d
Andrew Dillon <andrew.dillon.j@gmail.com>
2021-01-09 16:05:20 -0600

    Reset scratchpad

--- scratchpad/index.ts
+++ scratchpad/index.ts
@@ -3,11 +3,33 @@ import { openPdf, Reader } from './open';
 import { PDFDocument } from 'src/index';
 
 (async () => {
-  const pdfDoc = await PDFDocument.load(
-    fs.readFileSync('/Users/user/Desktop/s1.pdf'),
-  );
+  // This should be a Uint8Array or ArrayBuffer
+  // This data can be obtained in a number of different ways
+  // If your running in a Node environment, you could use fs.readFile()
+  // In the browser, you could make a fetch() call and use res.arrayBuffer()
+  const formPdfBytes = fs.readFileSync('assets/pdfs/form_to_flatten.pdf');
 
+  // Load a PDF with form fields
+  const pdfDoc = await PDFDocument.load(formPdfBytes);
+
+  // Get the form containing all the fields
   const form = pdfDoc.getForm();
+
+  // Fill the form's fields
+  form.getTextField('Text1').setText('Some Text');
+
+  form.getRadioGroup('Group2').select('Choice1');
+  form.getRadioGroup('Group3').select('Choice3');
+  form.getRadioGroup('Group4').select('Choice1');
+
+  form.getCheckBox('Check Box3').check();
+  form.getCheckBox('Check Box4').uncheck();
+
+  form.getDropdown('Dropdown7').select('Infinity');
+
+  form.getOptionList('List Box6').select('Honda');
+
+  // Flatten the form's fields
   form.flatten();
 
   // Serialize the PDFDocument to bytes (a Uint8Array)
