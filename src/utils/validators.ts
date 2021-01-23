653d664bc309647b61355b651d2148b85ccde513
Tim Knapp <tim@emergetec.com>
2021-01-23 15:43:34 +1100

    Fixing bug in validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -216,7 +216,7 @@ export const assertPositiveNum = (
 	value: number,
 	valueName: string
 ) => {
-	if ([1, 0].includes(Math.sign(value))) {
+	if (![1, 0].includes(Math.sign(value))) {
 		throw new Error(`${backtick(valueName)} must be a positive number or 0, but was actually ${value}`);
 	}
 };
