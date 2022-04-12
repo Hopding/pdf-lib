79a48684229467c68e31257a11c8a9e00bca2cbc
Reto Fahrni <82324518+retfah@users.noreply.github.com>
2022-04-12 08:11:17 +0200

    Update index.ts
    revert temporary test removal

--- apps/node/index.ts
+++ apps/node/index.ts
@@ -5,7 +5,7 @@ import { sep } from 'path';
 import readline from 'readline';
 
 import test1 from './tests/test1';
-/*import test2 from './tests/test2';
+import test2 from './tests/test2';
 import test3 from './tests/test3';
 import test4 from './tests/test4';
 import test5 from './tests/test5';
@@ -21,7 +21,7 @@ import test14 from './tests/test14';
 import test15 from './tests/test15';
 import test16 from './tests/test16';
 import test17 from './tests/test17';
-import test18 from './tests/test18';*/
+import test18 from './tests/test18';
 
 const cli = readline.createInterface({
   input: process.stdin,
@@ -162,8 +162,8 @@ const main = async () => {
 
     // prettier-ignore
     const allTests = [
-      test1//, test2, test3, test4, test5, test6, test7, test8, test9, test10,
-      //test11, test12, test13, test14, test15, test16, test17, test18, 
+      test1, test2, test3, test4, test5, test6, test7, test8, test9, test10,
+      test11, test12, test13, test14, test15, test16, test17, test18, 
     ];
 
     const tests = testIdx ? [allTests[testIdx - 1]] : allTests;
